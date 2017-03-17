bc9d1b8af7268db224d063e92d0fa61488b75eed
Caio <caiof.adriano@hotmail.com>
2017-03-17 10:41:07 -0300

    Criado base para validação do formulario de disciplinas

--- application/controllers/Disciplina.php
+++ application/controllers/Disciplina.php
@@ -8,7 +8,24 @@
   class Disciplina extends CI_Controller {
 
     public function index () {
-      
+
+    }
+
+    public function cadastro () {
+      // Carrega a biblioteca para validação dos dados.
+      $this->load->library('form_validation');
+
+      // Definir as regras de validação para cada campo do formulário.
+      $this->form_validation->set_rules('nomeCurso', 'nome do curso', array('required', 'min_length[5]'));
+      $this->form_validation->set_rules('cursoSigla', 'sigla', array('required', 'exact_length[3]'));
+      $this->form_validation->set_rules('qtdProfessores', '', array('required', 'integer'));
+
+      // Verifica se o formulario é valido
+      if ($this->form_validation->run() == FALSE) {
+        // TODO Criar lógica para formulario invalido
+      } else {
+        // TODO Criar lógica para formulário valido
+      }
     }
 
   }
