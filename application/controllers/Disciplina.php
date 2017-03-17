8e6e989bc6dc8efa7cd3ea7d503ae1ae882dd45b
Caio <caiof.adriano@hotmail.com>
2017-03-17 10:01:05 -0300

    Criado modelo de disciplina

---
+++ application/controllers/Disciplina.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+  /**
+   *  Essa classe é responsavel por todas regras de negócio sobre disciplinas.
+   *  @since 2017/03/17
+   *  @author Caio de Freitas
+   */
+  class Disciplina extends CI_Controller {
+
+    public function index () {
+      
+    }
+
+  }
+
+?>
