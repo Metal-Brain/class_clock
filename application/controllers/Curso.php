79c94380399380fcb93373fc5d67b0ec0fe17bb2
Caio <caiof.adriano@hotmail.com>
2017-03-20 13:46:55 -0300

    Criado base para controller e model

---
+++ application/controllers/Curso.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+  /**
+    * Essa classe contem todos as função de Curso
+    * @author Caio de Freitas
+    * @since 2017/03/20
+    */
+  class Curso extends CI_Controller {
+
+    public function cadastrar () {
+      
+    }
+
+    public function atualizar () {
+
+    }
+
+    public function deletar () {
+
+    }
+
+  }
+
+?>
