18a9054070347df0c9028a3600d0488ae6490736
Caio <caiof.adriano@hotmail.com>
2017-03-20 13:46:55 -0300

    Criado base para controller e model

---
+++ application/models/Curso_model.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+  /**
+    * Essa classe é um modelo que representa a relação Curso da base de dados.
+    * @author Caio de Freitas
+    * @since 2017/03/20
+    */
+  class Curso_model extends CI_Model {
+
+    function getCurso() {
+      // TODO: Criar função para buscar todos os cursos com o status de ativo
+    }
+
+    function getCursoById($idCurso) {
+      // TODO: Criar função para retornar o curso pelo ID informado.
+    }
+
+    function insertCurso($curso) {
+      // TODO: criar função para inserir um novo curso na base de dados.
+    }
+
+    function updateCurso($idCurso, $curso) {
+      // TODO: criar função para autalizar os dados do curso na base dados
+    }
+
+    function deleteCurso() {
+      // TODO: criar função para "deletar" o curso
+    }
+
+  }
+
+?>
