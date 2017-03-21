96be62e86b80eb210d35c0263990baf7a4a7b1cd
Caio <caiof.adriano@hotmail.com>
2017-03-21 13:52:08 -0300

    Criada função para alteração dos dados de disciplina

--- application/controllers/Disciplina.php
+++ application/controllers/Disciplina.php
@@ -67,6 +67,43 @@
       $this->Disciplina_model->deleteDisciplina($id);
     }
 
+    /**
+      * Altera os dado da disciplina.
+      * @author Caio de Freitas
+      * @since 2017/03/21
+      * @param $id ID da disciplina
+      */
+    public function atualizar ($id) {
+
+      $this->load->library('form_validation');
+      $this->load->model(array('Disciplina_model'));
+
+      // Definir as regras de validação para cada campo do formulário.
+      $this->form_validation->set_rules('nomeDisciplina', 'nome do curso', array('required','min_length[5]','ucwords'));
+      $this->form_validation->set_rules('sigla', 'sigla', array('required', 'exact_length[3]','strtoupper'));
+      // TODO adicionar a validação do curso
+      $this->form_validation->set_rules('nProfessores', 'quantidade de professores', array('required', 'integer', 'greater_than[0]'));
+      // Definição dos delimitadores
+      $this->form_validation->set_error_delimiters('<span class="error">','</span>');
+
+      // Verifica se o formulario é valido
+      if ($this->form_validation->run() == FALSE) {
+      } else {
+
+        // Pega os dados do formulário
+        $disciplina = array(
+          'nomeDisciplina'  => $this->input->post("nomeDisciplina"),
+          'sigla'           => $this->input->post('sigla'),
+          'nProfessores'    => $this->input->post("nProfessores")
+        );
+
+        $this->Disciplina_model->updateDisciplina($id, $disciplina);
+
+      }
+    }
+
+
+
   }
 
 ?>
