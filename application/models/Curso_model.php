02fa4457f19e0c177d1d1afa4e9ac4d39aa65b9e
Caio <caiof.adriano@hotmail.com>
2017-03-22 09:09:33 -0300

    Criado modelo fisico do banco de dados

--- application/models/Curso_model.php
+++ application/models/Curso_model.php
@@ -20,58 +20,55 @@
     }
 
     /**
-    *Essa função tem como objetivo encontrar um Curso
-    *pelo seu ID.
-    *@author Felipe Ribeiro da Silva
-    *@since 2017/03/21
-    *@param  $idCurso - ID de Cursos
-    *@return array de Cursos
+      * Essa função tem como objetivo encontrar um Curso
+      * pelo seu ID.
+      * @author Felipe Ribeiro da Silva
+      * @since 2017/03/21
+      * @param  $idCurso - ID de Cursos
+      * @return array de Cursos
     */
     function getCursoById($idCurso) {
-    $this->db->where('idCurso', $idCurso);
-    $result = $this->db->get('Curso');
+      $this->db->where('idCurso', $idCurso);
+      $result = $this->db->get('Curso');
 
-    return $result->result_array();
+      return $result->result_array();
     }
 
     /**
-    *Essa função insere um Curso.
-    *Caso esteja tudo correto, o curso
-    *será inserido na tabela.
-    *@author Felipe Ribeiro da Silva
-    *@since 2017/03/21
-    *@param  pega como paramentro $curso
-    *@return uma inserção na tabela curso.
+      * Essa função insere um Curso.
+      * Caso esteja tudo correto, o curso
+      * será inserido na tabela.
+      * @author Felipe Ribeiro da Silva
+      * @since 2017/03/21
+      * @param  pega como paramentro $curso
+      * @return uma inserção na tabela curso.
     */
     function insertCurso($curso) {
-    return $this->db->insert('Curso', $curso);
-
+      return $this->db->insert('Curso', $curso);
     }
 
     /**
-    *Atualiza os dados do usuario.
-    *Caso seja preenchido corretamente.
-    *os dados do usuario serão atualizados.
-    *@author Felipe Ribeiro da Silva
-    *@since 2017/03/21
-    *@param Int $idCurso - ID do Curso
+      * Atualiza os dados do usuario.
+      * Caso seja preenchido corretamente.
+      * os dados do usuario serão atualizados.
+      * @author Felipe Ribeiro da Silva
+      * @since 2017/03/21
+      * @param Int $idCurso - ID do Curso
     */
-
     function updateCurso($idCurso, $curso) {
-    $this->db->where('idCurso');
-    $result = $this->db->update('Curso', $curso);
+      $this->db->where('idCurso');
+      $result = $this->db->update('Curso', $curso);
 
-    return $result;
+      return $result;
     }
 
     /**
-    *Deleta um curso.
-    *@author Felipe Ribeiro da Silva
-    *@since 2017/03/21
-    *@param Int $idCurso - ID do Curso
-    *@return altera o boolean  para TRUE ou False dos Dados caso Seja deletado.
+      * Deleta um curso.
+      * @author Felipe Ribeiro da Silva
+      * @since 2017/03/21
+      * @param Int $idCurso - ID do Curso
+      * @return altera o boolean  para TRUE ou False dos Dados caso Seja deletado.
     */
-
     function deleteCurso($idCurso) {
       $this->db->where('idCurso', $idCurso);
       $result = $this->db->update('Curso', array('statusCurso' =>FALSE));
