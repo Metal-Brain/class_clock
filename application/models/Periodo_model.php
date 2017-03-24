3f262ab46802c5bd7c33624ba546b4ff496b1582
Caio <caiof.adriano@hotmail.com>
2017-03-24 14:01:05 -0300

    Criando controller para cadastro de curso

---
+++ application/models/Periodo_model.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+  /**
+    * Essa classe é uma modelo que representa a relação "Periodo" no banco de dados.
+    * @author Caio de Freitas
+    * @since 2017/03/24
+    */
+  class Periodo_model extends CI_Model {
+
+    /**
+      * Busca todos os periodos cadastrados na base de dados
+      * @author Caio de Freitas
+      * @since 2017/03/24
+      * @return Array - Retorna uma array com todos os periodos
+      */
+    public function getAll () {
+      $result = $this->db->get('Periodo');
+      return $result->result_array();
+    }
+
+  }
+
+?>
