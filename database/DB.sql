a9689a5697c5381331a72aa18cb3de5915b0b092
urielcaire <uriel.caire@gmail.com>
2017-03-28 13:13:17 -0300

    atualizando tabela disciplina do sql

--- database/DB.sql
+++ database/DB.sql
@@ -4,12 +4,12 @@ CREATE DATABASE IF NOT EXISTS CLASSCLOCK;
 USE CLASSCLOCK;
 
 CREATE TABLE IF NOT EXISTS Disciplina(
-	id             	INT         NOT NULL AUTO_INCREMENT,
-	nome           	VARCHAR(45) NOT NULL,
-	sigla          	VARCHAR(5)  NOT NULL,
-	qtdProf					INT         NOT NULL,
-	`status`       	BOOLEAN     NOT NULL DEFAULT TRUE,
-	PRIMARY KEY (id)
+ id                 INT           NOT NULL  AUTO_INCREMENT,
+ nome               VARCHAR(45)   NOT NULL,
+ sigla              VARCHAR(5)    NOT NULL  UNIQUE,
+ qtdProf            INT           NOT NULL,
+ status             BOOLEAN       NOT NULL  DEFAULT TRUE,
+ PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS Grau (
