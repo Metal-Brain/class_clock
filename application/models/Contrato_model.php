639d94f5c105bcd78394c815c2f37958adbfc808
jeanfbd <jeanfbd@hotmail.com>
2017-04-04 15:15:25 -0300

    Adicionado model Competencia, Contrato e Nivel corrigido chamada na view de Professores

---
+++ application/models/Contrato_model.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+  /**
+   *  Essa classe é um modelo que representa a relação Contrato no banco de dados
+   * @author Jean Brock
+   * @since 2017/04/04
+   */
+  class Contrato_model extends CI_Model {
+
+    /**
+      * Busca todos os graus cadastrados na base de dados.
+      * @author Jean Brock
+      * @since 2017/04/04
+      * @return Array - Retorna um array com todos o contratos
+      */
+    public function getAll () {
+      $result = $this->db->get('Contrato');
+      return $result->result_array();
+    }
+  }
+
+
+?>
