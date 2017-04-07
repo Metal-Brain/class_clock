e185bb6f0ba630b8f819ee74b3a6ace155bb40e4
YASMIN <YASMIN>
2017-04-06 21:07:10 -0300

    Alteracoes na view

--- application/views/preferencias.php
+++ application/views/preferencias.php
@@ -35,100 +35,29 @@
 				</div>
 
 				<div id="content" class="col-md-10">
-					<h1>Preferência</h1>
-						<form>
-								<div class="modal-content col-md-offset-3 col-md-5">
-								 <div class="form-group disc">
-										<label>Disciplinas </label><button onClick="addSelect()" type="button" class="btn btn-success glyphicon glyphicon-plus"></button> 
-										
-										<br>
-										
-										<div id="selects">
-											<div id="select-disciplina" class="form-group s-disciplina">
-												<select class="form-control">
-													<option value="Lógica de Programação">Lógica de Programação</option>
-													<option value="Estrutura de Dados">Estrutura de Dados</option>
-													<option value="Linguagem de Programação">Linguagem de Programação</option>
-													<option value="Banco de Dados">Banco de Dados</option>
-												</select>
-											</div>
-										</div>
-										
-										<button id="btn-remover" onClick="remSelect()" type="button" class="btn btn-danger" style="display: none;">Remover</button>
-										<button id="btn-Limpar" onClick="pt()" type="button" class="btn btn-danger" style="display: none;">Limpar</button>
-									</div>
+					<h1>Preferências</h1>
+					<form>
+						<div class="modal-content col-md-offset-3 col-md-5">
+							<div class="form-group disc">
+								<label>Disciplinas</label>
+								
+								<!-- Aqui entra o multi select -->
 
-									<div class="form-group">
-										<button type='button' class='btn bt-lg btn-primary'>Cadastrar</button>
-									</div>
+								<div class="form-group">
+									<button type='button' class='btn bt-lg btn-primary'>Cadastrar</button>
 								</div>
-						</form>	
-
-			
-
-				</div>
-				<div id="footer" class="col">
-					<p>Desenvolvido por Metal Code</p>
-				</div>
-			</div>
-			
+							</div>
+						</div>
+					</form>
+						
+					<div id="footer" class="col">
+						<p>Desenvolvido por Metal Code</p>
+					</div>
+				</div><!--Fecha content-->
+			</div><!--Fecha row-->
 		</div><!--Fecha container-fluid-->
-		<script type="text/javascript" src="assets/js/jquery-3.1.1.min.js"></script>
-		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-		 <script>
-            //faz a contagens de divs com uma determinada classe
-            function contaClasse(classe) {
-                return document.getElementsByClassName(classe).length
-            }
-
-            // adiciona um select dentro da div 'selects'
-            function addSelect() {
-                //pega o elemento/div que possui o id 'select-disciplina'
-                var select = document.getElementById('select-disciplina')
-				
-				
-				
-				
-                //pega o elemento/div que possui o id 'selects'
-                var area = document.getElementById('selects')
-                //faz a copia do elemento capturado na var select ( o 'select-disciplina')
-                var clone = select.cloneNode(true)
-                //insere o clone dentro da elemento da variavel area (o 'selects')
-                area.appendChild(clone)
-
-                //se quantidade de classes 's-disciplina' > 1 ele exibe o botao remover
-                if (contaClasse('s-disciplina') > 1) {
-				
-                    document.getElementById('btn-remover').style.display = 'inline'
-					document.getElementById('btn-Limpar').style.display = 'inline'
-					}
-					
-            }
-            function remSelect() {
-                //remove o ultimo elemento com id 'select-disciplina'
-              document.querySelector('#select-disciplina:last-child').remove()
-				
-			
-
-                //se quantidade de classes 's-disciplina' === 1 esconde o botao remover
-                if (contaClasse('s-disciplina') === 1) {
-					document.getElementById('btn-remover').style.display = 'none'
-					document.getElementById('btn-Limpar').style.display = 'none'
-				}
-                 
-            }
-			
-			//Remove tudo e só deixa o minimo para sobreviver
-			function pt() {
-				do {
-						document.querySelector('#select-disciplina:last-child').remove()
-					} while(contaClasse('s-disciplina') > 1)
-					
-					document.getElementById('btn-remover').style.display = 'none'
-					document.getElementById('btn-Limpar').style.display = 'none'
-					
-			}
-        </script>
 		
+		<script type="text/javascript" src="<?= base_url('assets/js/jquery-3.1.1.min.js')?>"></script>
+		<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js')?>"></script>
 	</body>
 </html>
