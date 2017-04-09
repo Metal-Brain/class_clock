5e142b2ee80d460e8e03165f53a3d3ca594cd270
minska13 <yasmin.sayad@gmail.com>
2017-04-09 20:10:13 -0300

    Correcao do titulo da pagina

--- application/views/preferencias.php
+++ application/views/preferencias.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Professor</title>
+		<title>Preferências</title>
 		<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css')?>">
 		<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/style.css')?>">
 		<!-- Multi-Select -->
