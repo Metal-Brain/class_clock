496580f67e45b4700f47adb7ea6bcd7600149bf5
Felipe <felipe.rtec@gmail.com>
2017-04-10 07:48:53 -0300

    Subindo arquivos

---
+++ application/models/Competencia_model.php
@@ -0,0 +1,50 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+  /**
+    * Essa classe representa o modelo da relação Curso_tem_Disciplina.
+    * @author Jean Brock
+    * @since 2017/04/04
+    */
+  class Competencia_model extends CI_Model {
+    /**
+      * Insere no banco de dados os dado do relacionamento entre professor e
+      * disciplina.
+      * @author Jean Brock
+      * @since 2017/04/04
+      * @param INT $professor - ID do professor
+      * @param INT $disciplina - ID da disciplina
+      * @return boolean - Retorna TRUE caso os dados sejam inseridos no banco
+      * de dados com sucesso.
+      */
+    public function insert ($professor, $disciplina) {
+      return $this->db->insert('Competencia',array(
+        'idProfessor'       => $professor,
+        'idDisciplina'  => $disciplina
+      ));
+    }
+    /**
+      * Deleta um relação entre professor e disciplina
+      * @author Jean Brock
+      * @since 2017/04/04
+      * @param INT $professor - ID do professor
+      * @return Retorna um boolean True caso a relação foi desfeita
+      */
+    public function delete ($professor) {
+      $this->db->where('idProfessor', $professor);
+      $result = $this->db->delete('Competencia');
+      return $result;
+    }
+    /**
+      * @author Jean Brock
+      * @since 2017/04/04
+      * @param INT $professor - ID do professor
+      * @return Retorna um array com todas as disciplinas
+      */
+    public function getAllDisciplinas($professor) {
+      $this->db->select('Disciplina.*');
+      $this->db->where('idProfessor',$professor);
+      $this->db->join('Disciplina','Disciplina.id = Competencia.idDisciplina');
+      $result = $this->db->get('Competencia');
+      return $result->result_array();
+    }
+  }
+?>
--- database/DB.sql
+++ database/DB.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS Curso (
     FOREIGN KEY (grau) REFERENCES Grau(id)
 );
 
- CREATE TABLE IF NOT EXISTS Curso_tem_disciplina (
+CREATE TABLE IF NOT EXISTS Curso_tem_disciplina (
    idDisciplina INT NOT NULL,
    idCurso      INT NOT NULL,
    CONSTRAINT fk_disciplina_curso
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS Curso (
     FOREIGN KEY (idCurso) REFERENCES Curso(id)
  );
 
- CREATE TABLE IF NOT EXISTS Curso_tem_Periodo (
+CREATE TABLE IF NOT EXISTS Curso_tem_Periodo (
 	 idCurso			INT NOT NULL,
 	 idPeriodo		INT NOT NULL,
 	 PRIMARY KEY (idCurso,idPeriodo),
@@ -58,12 +58,51 @@ CREATE TABLE IF NOT EXISTS Curso (
 	 CONSTRAINT fk_periodo_curso
 	 	FOREIGN KEY (idPeriodo) REFERENCES Periodo(id)
  );
- 
- CREATE TABLE IF NOT EXISTS Sala(
-	id 					INT NOT NULL 			AUTO_INCREMENT,
-	nSala 				VARCHAR(5) NOT NULL UNIQUE,
-	capMax			 	INT NOT NULL,
-	tipo 				VARCHAR(45) NOT NULL,
-	`status` 			BOOLEAN       NOT NULL  DEFAULT TRUE,
+
+CREATE TABLE IF NOT EXISTS Sala (
+	id 					   INT            NOT NULL 			AUTO_INCREMENT,
+	nSala 				 VARCHAR(5)     NOT NULL      UNIQUE,
+	capMax			 	 INT            NOT NULL,
+	tipo 				   VARCHAR(45)    NOT NULL,
+	`status` 			 BOOLEAN        NOT NULL      DEFAULT TRUE,
 	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS Contrato(
+	id					INT           NOT NULL 			AUTO_INCREMENT,
+  nome 				VARCHAR(45) 	NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO Contrato (nome) VALUES ('Exclusiva'),('Integral'),('Parcial');
+
+CREATE TABLE IF NOT EXISTS Nivel (
+	id					INT 			    NOT NULL 			AUTO_INCREMENT,
+  nome 				VARCHAR(45) 	NOT NULL,
+  PRIMARY KEY(id)
+);
+
+INSERT INTO Nivel (nome) VALUES ('Graduação'),('Pós-Graduação'),('Mestrado'),('Doutorado');
+
+CREATE TABLE IF NOT EXISTS Professor(
+	id 					INT 			    NOT NULL 			AUTO_INCREMENT,
+  nome 				VARCHAR(255) 	NOT NULL,
+  matricula 	CHAR(7)    		NOT NULL			UNIQUE,
+  nascimento 	DATE 			    NOT NULL,
+  coordenador tinyint(1) 		NOT NULL			DEFAULT 0,
+  `status` 		tinyint(1) 		NOT NULL			DEFAULT 1,
+  idContrato 	INT 			    NOT NULL,
+  idNivel			INT 			    NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(idContrato) REFERENCES Contrato(id),
+	FOREIGN KEY(idNivel) REFERENCES Nivel(id)
+);
+
+CREATE TABLE IF NOT EXISTS Competencia(
+	idProfessor			INT 			    NOT NULL,
+  idDisciplina		INT 			    NOT NULL,
+  interesse 			tinyint(1) 		NOT NULL			DEFAULT 0,
+  PRIMARY KEY(idProfessor, idDisciplina),
+  FOREIGN KEY(idProfessor) REFERENCES Professor(id),
+  FOREIGN KEY(idDisciplina) REFERENCES Disciplina(id)
 );
