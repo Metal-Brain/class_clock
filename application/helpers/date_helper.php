487eb9ede9ebf71bf280e97c1f9f28cbbc831d3c
Felipe <felipe.rtec@gmail.com>
2017-04-10 08:11:55 -0300

    Corrigindo alguns bugs

---
+++ application/helpers/date_helper.php
@@ -0,0 +1,24 @@
+<?php
+  /**
+    * Converte uma data em formato BR para SQL
+    * @author Caio de Freitas
+    * @since 2017/04/03
+    * @param String $date - Data em formato BR
+    * @return Retorna uma string com a data em formato SQL
+    */
+  function brToSql ($date) {
+    $d = explode('/',$date);
+    return $d[2].'-'.$d[1].'-'.$d[0];
+  }
+  /**
+   * Converte uma data em formato SQL para BR
+   * @author Caio de Freitas
+   * @since 2017/04/06
+   * @param String $date - Data em formato SQL
+   * @return Retorna uma string com a data em formato BR
+   */
+  function sqlToBr ($date) {
+    $d = explode('-',$date);
+    return $d[2].'/'.$d[1].'/'.$d[0];
+  }
+?>
---
+++ application/models/Nivel_model.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+  /**
+   *  Essa classe é um modelo que representa a relação Nivel no banco de dados
+   * @author Jean Brock
+   * @since 2017/04/04
+   */
+  class Nivel_model extends CI_Model {
+    /**
+      * Busca todos os niveis cadastrados na base de dados.
+      * @author Jean Brock
+      * @since 2017/04/04
+      * @return Array - Retorna um array com todos o niveis.
+      */
+    public function getAll () {
+      $result = $this->db->get('Nivel');
+      return $result->result_array();
+    }
+  }
+?>
