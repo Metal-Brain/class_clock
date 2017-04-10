62cf7d733e0e079d52b6f9d05d7e1aa1b9ec1f6b
Felipe <felipe.rtec@gmail.com>
2017-04-10 08:11:55 -0300

    Corrigindo alguns bugs

--- application/helpers/date_helper.php
+++ application/helpers/date_helper.php
@@ -1,5 +1,8 @@
 <?php
+<<<<<<< 27842a51129a7cfe55853c96b70a930dae567d93
 
+=======
+>>>>>>> Corrigindo alguns bugs
   /**
     * Converte uma data em formato BR para SQL
     * @author Caio de Freitas
@@ -11,7 +14,10 @@
     $d = explode('/',$date);
     return $d[2].'-'.$d[1].'-'.$d[0];
   }
+<<<<<<< 27842a51129a7cfe55853c96b70a930dae567d93
 
+=======
+>>>>>>> Corrigindo alguns bugs
   /**
    * Converte uma data em formato SQL para BR
    * @author Caio de Freitas
@@ -23,5 +29,8 @@
     $d = explode('-',$date);
     return $d[2].'/'.$d[1].'/'.$d[0];
   }
+<<<<<<< 27842a51129a7cfe55853c96b70a930dae567d93
 
+=======
+>>>>>>> Corrigindo alguns bugs
 ?>
--- application/models/Nivel_model.php
+++ application/models/Nivel_model.php
@@ -18,10 +18,5 @@
       return $result->result_array();
     }
 
-
   }
-
-
-
-
 ?>
