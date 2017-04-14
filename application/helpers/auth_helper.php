137ceeeb9aaef6e7d93e259eb2c494b0be82d148
Caio de Freitas <caiof.adriano@hotmail.com>
2017-04-14 17:21:49 -0300

    Fix the authentication helper

--- application/helpers/auth_helper.php
+++ application/helpers/auth_helper.php
@@ -1,15 +1,5 @@
 <?php
 
-function autoriza(){
-  $CI = get_instance();
-  $usuarioLogado = $CI->session->userdata('usuario_logado');
-  if (!$usuarioLogado) {
-    $CI->session->set_flashdata("danger","Você precisa estar logado");
-  }
-  return $usuarioLogado;
-}
-
-
 function verificaSessao () {
   $CI = get_instance();
   $usuario = $CI->session->userdata();
@@ -19,5 +9,5 @@ function verificaSessao () {
 
 function verificaNivelPagina($niveis) {
   $CI = get_instance();
-  return ((in_array($CI->session->nivel, $niveis) == 1) ? TRUE : FALSE);
+  return (array_search($CI->session->nivel, $niveis) === false) ? FALSE : TRUE;
 }
