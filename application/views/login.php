02f156d0abfa0ceb4f32ef65dd8134bb1223d392
felipe <felipe.rtec@gmail.com>
2017-04-15 10:24:58 -0300

    Deixado em comentario a opção de recuperar a senha

--- application/views/login.php
+++ application/views/login.php
@@ -54,7 +54,7 @@
 										<?php endif; ?>
 
 										<button type="submit" class="btn btn-def btn-block" style="background-color: #4CAF50">Entrar</button>
-										<a  data-toggle="pill" style="text-align:center;display:block;" href="#recupera">Clique aqui para recuperar a senha</a>
+										<!-- <a  data-toggle="pill" style="text-align:center;display:block;" href="#recupera">Clique aqui para recuperar a senha</a> -->  <!-- OBS: Liberar essa opção só quando começar a ser implementada!!! -->
 									</div>
 								<?= form_close() ?>
 							</div>
@@ -63,7 +63,7 @@
 							<!-- modal da recuperação de senha -->
 							<div id="recupera" class="col-md-6 col-md-offset-3 tab-pane fade">
 								<h2 style="text-align:center">Recuperar Senha</h2>
-							
+
 								<div class="help">
 									<p><strong>Ex: 0000000 </strong> Informe seu login, instruções serão enviadas para o<br> e-mail cadastrado em nosso sistema, se necessário, procure a CTI.</p>
 								</div>
@@ -71,11 +71,11 @@
 									<div class="form-group input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 										<input class="form-control" type="text" name='username' placeholder="ex: 0000000"/>
-								
+
 									</div>
-									
-									<div class="form-group">										
-										
+
+									<div class="form-group">
+
 											<!-- Alert de sucesso -->
 										<?php if ($this->session->flashdata('success')) : ?>
 											<div class="text-center alert alert-success" role="alert">
@@ -98,19 +98,19 @@
 												<?= validation_errors() ?>
 											</div>
 										<?php endif; ?>
-										
+
 									</div>
 
 									<div class="col-md-6 col-md-offset-3 inline">
-										
-									
-										
+
+
+
 										<button type='submit' class='btn bt-lg btn-primary' style="background-color: #4CAF50">Redefinir</button>
 										<button type='button' class='btn bt-lg btn-default'  data-toggle="pill" href="#principal">Cancelar</button>
 									</div>
 								</form>
 							</div>
-							
+
 							<!-- Fim modal da recuperação de senha -->
 						</div>
 
