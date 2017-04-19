d225e9763889dfee548b4f3d4101f53cf036c29e
felipe <felipe.rtec@gmail.com>
2017-04-19 20:08:53 -0300

    Atualizando branch

--- application/models/Professor_model.php
+++ application/models/Professor_model.php
@@ -1,18 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-<<<<<<< HEAD
 
-=======
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
   /**
    *  Esse modelo possui uma serie de funções sob a relação de professores.
    *  @author Yasmin Sayad
    *  @since 2017/04/03
    */
   class Professor_model extends CI_Model {
-<<<<<<< HEAD
 
-=======
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
     /**
       * Busca todos os professores cadastrados na base de dados.
       * @author Yasmin Sayad
@@ -24,16 +18,12 @@
       $this->db->join('Contrato','Contrato.id = Professor.idContrato');
       $this->db->join('Nivel','Nivel.id = Professor.idNivel');
       $result = $this->db->get('Professor');
-<<<<<<< HEAD
 
       return $result->result_array(); // converte o objeto em um array
     }
 
 
-=======
-      return $result->result_array(); // converte o objeto em um array
-    }
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
+
     /**
       * Busca o professor a partir do ID
       * @author Yasmin Sayad
@@ -44,16 +34,12 @@
     public function getById ($idProfessor) {
       $this->db->where('id', $idProfessor);
       $result = $this->db->get('Professor');
-<<<<<<< HEAD
 
       return $result->result_array();
     }
 
 
-=======
-      return $result->result_array();
-    }
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
+
     /**
       * Insere um novo professor na base de dados.
       * @author Yasmin Sayad
@@ -64,10 +50,7 @@
     public function insert ($professor) {
       return $this->db->insert('Professor', $professor);
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
     /**
       * Atualiza os dados de um professor
       * @author Yasmin Sayad
@@ -78,16 +61,12 @@
     public function update ($idProfessor, $professor) {
       $this->db->where('id', $idProfessor);
       $result = $this->db->update('Professor', $professor);
-<<<<<<< HEAD
 
       return $result;
     }
 
 
-=======
-      return $result;
-    }
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
+
     /**
       * Altera o status de um professor para falso.
       * @author Yasmin Sayad
@@ -98,16 +77,12 @@
     public function disable ($id) {
       $this->db->where('id', $id);
       $result = $this->db->update('Professor',array('status'=>FALSE));
-<<<<<<< HEAD
 
       return $result;
     }
 
 
-=======
-      return $result;
-    }
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
+
     /**
       * Altera o status do professor para TRUE.
       * @author Yasmin Sayad
@@ -118,7 +93,6 @@
     public function able ($id) {
       $this->db->where('id',$id);
       $result = $this->db->update('Professor',array('status'=>TRUE));
-<<<<<<< HEAD
 
       return $result;
     }
@@ -127,9 +101,5 @@
 
 
 
-=======
-      return $result;
-    }
-  }
->>>>>>> 1979f79d960aa0ab3b35bc6e265fd19826e5519e
+
 ?>
