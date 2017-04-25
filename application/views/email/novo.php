15374c6936852facd2bff3c76c2fd76b7c7aaf8c
minska13 <yasmin.sayad@gmail.com>
2017-04-25 16:24:50 -0300

    Alteracao da view email novo

--- application/views/email/novo.php
+++ application/views/email/novo.php
@@ -1,86 +1,67 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <style>
-      html, body {
-        margin: 0;
-        padding: 0;
-      }
-	  
-	  h3{
-	  
-	  color:rgb(112,112,112);
-	  text-align:left;
-	  }
-      .page {
-        width: 100%;
-        min-width: 300px;
-        margin: 10px 25px;
-        height: auto;
-        border-radius: 3%;
-      }
-   
-      .table {
-        border-collapse: collapse;
-        margin: 0 auto;
-      }
-      .table td {
-        padding: 2px 3px;
-      }
-	  .footer{
-	  width:100%;
-	
-	    color:rgb(112,112,112);
-		margin: 30px 25px;
-	  }
-	  
-	  
-	  span{
-	  font-size:41px;
-	  color: rgb(38,114,236);
-	  
-	  }
-	  
-	  p span {
-	  	  font-size:21px;
-	  color: green;
-	  
-	  }
-	  
-	  
-	  .agradecimento{
-	  text-align:left;
-	  margin:5px 0px;
-	  
-	  }
-	  
-	  
-	  
-    </style>
-  </head>
-  <body>
-
-    <div class="page">
-	
-			<h3>Conta Class Clock</h3>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+			<style>
+			html, body{
+				margin: 0;
+				padding: 0;
+			}
+		  
+			h3{
+				color:rgb(112,112,112);
+				text-align:left;
+			}
 			
-		<span>Sua senha foi redefinida</span>
-			<p>Olá <?= $professor['nome'] ?> </p>
-			<p>A senha da sua conta Class Clock foi redefinida com sucesso!</p>
-			<p>Para acessar sua conta utilize a matricula <?= $professor['matricula'] ?> e a sua nova senha de acesso é <span> <?= $professor['senhaLimpa'] ?> </span> .</p>
+			p span{
+				font-weight: bold;
+				font-size: 15px;
+			}
+		  
+			.page{
+				width: 100%;
+				min-width: 300px;
+				margin: 10px 25px;
+				height: auto;
+				color: black;
+			}
 			
+			.ola{
+				font-size: 22px;
+			}
+			
+			.agradecimento{
+				text-align:left;
+				margin:5px 0px;
+			}
+		  
+			.footer{
+				width:100%;
+				height: 40px;
+				background: #CCC;
+				color:rgb(112,112,112);
+				margin: 15px 25px;
+				padding: 1px 0;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="page">
+			<h3>Class Clock</h3>
+			
+			<p class="ola">Olá <?= $professor['nome'] ?>, </p>
+			<p>Sua conta Class Clock foi criada com sucesso!</p>
+			<p>Para fazer login utilize a matrícula <?= $professor['matricula'] ?> e a senha de acesso <span> <?= $professor['senhaLimpa'] ?> </span></p>
+				
 
-     <p>Para acessar o sistema <?= anchor('/', 'clique aqui',array('target'=>'_blank')); ?></p>
-   <p class="agradecimento">Obrigado,</p>
-   <p class="agradecimento">Equipe Class Clock.</p>
-
-
-   </div>
-	<div class="footer">
-	
-		<p>Desenvolvido por Metal Code</p>
-	</div>
-  </body>
+			<p>Para acessar o sistema <?= anchor('/', 'clique aqui',array('target'=>'_blank')); ?>.</p>
+			<p class="agradecimento">Obrigado,</p>
+			<p class="agradecimento">Equipe Class Clock.</p>
+		</div>
+		<div class="footer">
+			<p>Desenvolvido por Metal Code</p>
+		</div>
+	</body>
 </html>
