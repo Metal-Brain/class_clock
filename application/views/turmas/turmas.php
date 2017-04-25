b87f59ec99adcdf758b3169764d31595ea107156
urielcaire <uriel.caire@gmail.com>
2017-04-25 10:58:13 -0300

    corrige botao exclude turma

--- application/views/turmas/turmas.php
+++ application/views/turmas/turmas.php
@@ -60,7 +60,7 @@
 								<td>
 									<?php if ($turma['status']): ?>
 										<button type="button" class="btn btn-warning" title="Editar" data-toggle="modal" data-target="#exampleModal"  data-whateverdisciplina="<?= $turma['disciplinaSigla']?>" data-whateverid="<?= $turma['id']?>"  data-whateversigla="<?= $turma['sigla']?>"  data-whateverqtd="<?= $turma['qtdAlunos']?>" data-whateverdp="<?= $turma['dp']?>"><span class="glyphicon glyphicon-pencil"></span></button>
-										<button onClick="disable(<?= $turma['id']?>)" type="button" class="btn btn-danger delete" title="Desativar"><span class="glyphicon glyphicon-remove"></span></button>
+										<button onClick="exclude(<?= $turma['id']?>)" type="button" class="btn btn-danger delete" title="Desativar"><span class="glyphicon glyphicon-remove"></span></button>
 									<?php else : ?>
 										<button onClick="able(<?= $turma['id']?>)" type="button" class="btn btn-success delete" title="Ativar"><span class="glyphicon glyphicon-ok"></span></button>
 									<?php endif; ?>
