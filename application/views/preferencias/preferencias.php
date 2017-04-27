d66b3d74c5ae20166564743b673312266621c264
minska13 <yasmin.sayad@gmail.com>
2017-04-27 11:33:54 -0300

    Correcoes dos erros gerados pelo merge

--- application/views/preferencias/preferencias.php
+++ application/views/preferencias/preferencias.php
@@ -1,35 +1,35 @@
-		<div id="content" class="col-md-10">
+	<div id="content" class="col-md-10">
 
-			<?php if($this->session->flashdata('success')) : ?>
-				<div class="alert alert-success">
-					<p class="text-center"><?= $this->session->flashdata('success') ?></p>
-				</div>
-			<?php endif; ?>
+		<?php if($this->session->flashdata('success')) : ?>
+			<div class="alert alert-success">
+				<p class="text-center"><?= $this->session->flashdata('success') ?></p>
+			</div>
+		<?php endif; ?>
 
-			<!-- Cadastro das preferências do professor -->
-			<?= form_open('Professor/preferencia') ?>
-				<div class="col-md-12">
-					<h1>Preferências</h1>
-					
-					<div class="row">
-						<!-- Aqui entra o multi select -->
-						<div class="form-group col-md-5">
-							<label>Disciplinas que deseja lecionar</label>
-							<?= form_dropdown('disciplinas[]',$disciplinas,null,array('id'=>'disciplinas','multiple'=>'multiple')) ?>
-							
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="col-md-8 margin-top-error">
-							<?= form_error('disciplinas[]') ?>
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<?= form_submit('submit','Cadastrar',array('class'=>'btn btn-primary')) ?>
+		<!-- Cadastro das preferências do professor -->
+		<?= form_open('Professor/preferencia') ?>
+			<div class="col-md-12">
+				<h1>Preferências</h1>
+				
+				<div class="row">
+					<!-- Aqui entra o multi select -->
+					<div class="form-group col-md-5">
+						<label>Disciplinas que deseja lecionar</label>
+						<?= form_dropdown('disciplinas[]',$disciplinas,null,array('id'=>'disciplinas','multiple'=>'multiple')) ?>
+						
 					</div>
 				</div>
-			<?= form_close() ?>
-		</div><!--Fecha content-->
+				
+				<div class="row">
+					<div class="col-md-8 margin-top-error">
+						<?= form_error('disciplinas[]') ?>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<?= form_submit('submit','Cadastrar',array('class'=>'btn btn-primary')) ?>
+				</div>
+			</div>
+		<?= form_close() ?>
+	</div><!--Fecha content-->
 </div><!--Fecha container-fluid-->
