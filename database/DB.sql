98a729b186e6af5c2b606cbb53f371cbb94f6b44
jeanfbd <jeanfbd@hotmail.com>
2017-05-08 14:16:55 -0300

    Criado função limitar disponibilidade por dia

--- database/DB.sql
+++ database/DB.sql
@@ -141,6 +141,7 @@ CREATE TABLE IF NOT EXISTS Disponibilidade(
     inicio 				TIME 			NOT NULL,
     fim 				TIME 			NOT NULL,
     `status`      		BOOLEAN     	NOT NULL  DEFAULT TRUE,
+    PRIMARY KEY(idPeriodo,idProfessor,dia,inicio,fim),
 
     FOREIGN KEY(idPeriodo) REFERENCES Periodo(id),
     FOREIGN KEY(idProfessor) REFERENCES Professor(id)
