c9cdd9818da5e5f23f506e7cfd812cff59379a87
felipemrk2e <felipemrk2e@gmail.com>
2017-05-17 20:50:38 -0300

    Adicionado modal de visualização em salas e as regras de nivel 2 temporariamente

--- application/views/salas/js_salas.php
+++ application/views/salas/js_salas.php
@@ -14,6 +14,24 @@
         modal.find('#recipient-capMax').val(recipientcapmax)
         modal.find('#recipient-tipo').val(recipienttipo)
         modal.find('#recipient-id').val(recipientId)
+    });
+	
+	
+	$('#exampleModal2').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var recipient = button.data('whatever') // Extract info from data-* attributes
+        var recipientnsala = button.data('whatevernsala')
+        var recipientcapmax = button.data('whatevercapmax')
+        var recipienttipo = button.data('whatevertipo')
+        var recipientId = button.data('whateverid')
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('.modal-title').text('Alterar Sala')
+        modal.find('#recipient-nSala').val(recipientnsala)
+        modal.find('#recipient-capMax').val(recipientcapmax)
+        modal.find('#recipient-tipo').val(recipienttipo)
+        modal.find('#recipient-id').val(recipientId)
     })
 </script>
 
--- application/views/salas/salas.php
+++ application/views/salas/salas.php
@@ -25,13 +25,14 @@
     <h1>Salas</h1>
 
     <!-- Lista de 'botoes' links do Bootstrap -->
+	<?php if ($this->session->nivel == 1) :?>
     <ul class="nav nav-pills">
         <!-- 'botao' link para a listagem -->
         <li class="active"><a data-toggle="pill" href="#list">Listar todas</a></li>
         <!-- 'botao' link para novo registro -->
         <li><a data-toggle="pill" href="#new">Cadastrar</a></li>
     </ul>
-
+	<?php endif;?>
     <!-- Dentro dessa div vai o conteúdo que os botões acima exibem ou omitem -->
     <div class="tab-content">
 
@@ -59,8 +60,15 @@
 							endif; ?></td>
 							<td>
 								<?php if ($sala['status']): ?>
+									<?php if ($this->session->nivel == 1) :?>
 									<button type="button" class="btn btn-warning" title="Editar" data-toggle="modal" data-target="#exampleModal" data-whatevernsala="<?= $sala['nSala']; ?>" data-whateverid="<?= $sala['id']; ?>" data-whatevercapmax="<?= $sala['capMax']; ?>"  data-whatevertipo="<?= $sala['tipo']; ?>"><span class="glyphicon glyphicon-pencil"></span></button>
 									<button onClick="exclude(<?= $sala['id'] ?>);" type="button" class="btn btn-danger" title="Desativar"><span class="glyphicon glyphicon-remove"></span></button>
+									<?php endif;?>
+									<?php if ($this->session->nivel == 2) :?>
+									<button type="button" class="btn btn-primary" title="Editar" data-toggle="modal" data-target="#exampleModal2" data-whatevernsala="<?= $sala['nSala']; ?>" data-whateverid="<?= $sala['id']; ?>" data-whatevercapmax="<?= $sala['capMax']; ?>"  data-whatevertipo="<?= $sala['tipo']; ?>"><span class="glyphicon glyphicon-eye-open"></span></button>
+									<?php endif;?>
+									
+									
 								<?php else : ?>
 									<button onClick="able(<?= $sala['id'] ?>)" type="button" class="btn btn-success delete" title="Ativar"><span class="glyphicon glyphicon-ok"></span></button>
 								<?php endif; ?>
@@ -195,6 +203,78 @@
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary">Alterar</button>
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                </div>
+
+				<?= form_close() ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Aqui é o Modal2 de alteração das salas-->
+<div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content col-md-12">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="exampleModalLabel">Salas</h4>
+            </div>
+            <div class="modal-body">
+
+				<?= form_open('Sala/atualizar') ?>
+
+                <div class="form-group">
+                    <input type="hidden" name="recipient-id" id="recipient-id">
+                </div>
+
+				<div class="row">
+					<div class="form-group col-md-2">
+						<label for="nSala-name" class="control-label">Sala</label>
+						<input type="number" min="0" class="form-control" name="recipient-nSala" id="recipient-nSala" required readonly/>
+					</div>
+                </div>
+
+				<div class="row">
+					<div class="col-md-6 margin-top-error">
+						<?= form_error('recipient-nSala') ?>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="form-group col-md-4">
+						<label for="tipo-name" class="control-label">Tipo</label>
+						<select  class="form-control" name="recipient-tipo" id="recipient-tipo" disabled>
+							<option>Laboratório</option>
+							<option>Teórica</option>
+						</select>
+					</div>
+                </div>
+
+				<div class="row">
+					<div class="col-md-6 margin-top-error">
+						<?= form_error('recipient-tipo') ?>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4">
+						<label for="capMax-name" class="control-label">Capacidade Máxima</label>
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col-md-2">
+						<input type="number" max="999" maxlength="3" pattern="[0-9]+$"class="form-control" name="recipient-capMax" id="recipient-capMax" required readonly/>
+					</div>
+                </div>
+
+				<div class="row">
+					<div class="col-md-7 margin-top-error">
+						<?= form_error('recipient-capMax') ?>
+					</div>
+				</div>
+
+                <div class="modal-footer">
+                           <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
                 </div>
 
 				<?= form_close() ?>
