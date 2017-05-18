2a1460325a31027ade16444018c7b07a96dc59ed
felipemrk2e <felipemrk2e@gmail.com>
2017-05-17 21:14:59 -0300

    Feito o mesmo que o commit anterior na view da cursos

--- application/views/cursos/cursos.php
+++ application/views/cursos/cursos.php
@@ -25,12 +25,14 @@
     <h1>Cursos</h1>
 
     <!-- Lista de 'botoes' links do Bootstrap -->
-    <ul class="nav nav-pills">
-        <!-- 'botao' link para a listagem -->
-        <li class="active"><a data-toggle="pill" href="#list">Listar todos</a></li>
-        <!-- 'botao' link para novo registro -->
-        <li><a data-toggle="pill" href="#new">Cadastrar</a></li>
-    </ul>
+	<?php if ($this->session->nivel == 1) :?>
+		<ul class="nav nav-pills">
+			<!-- 'botao' link para a listagem -->
+			<li class="active"><a data-toggle="pill" href="#list">Listar todos</a></li>
+			<!-- 'botao' link para novo registro -->
+			<li><a data-toggle="pill" href="#new">Cadastrar</a></li>
+		</ul>
+	<?php endif;?>
 
     <!-- Dentro dessa div vai o conteúdo que os botões acima exibem ou omitem -->
     <div class="tab-content">
@@ -64,9 +66,14 @@
                                     ?></td>
                                 <td>
                                     <?php if ($curso['status']): ?>
-                                        <button type="button" class="btn btn-warning" title="Editar" data-toggle="modal" data-target="#exampleModal" data-whateverid="<?= $curso['id'] ?>" data-whateversigla="<?= $curso['sigla'] ?>" data-whatevernome="<?= $curso['nome'] ?>" data-whateversemestres="<?= $curso['qtdSemestres'] ?>" data-whatevergrau="<?= $curso['grau'] ?>" data-whateverperiodo="<?= $curso['idPeriodo'] ?>"><span class="glyphicon glyphicon-pencil"></span></button>
-                                        <button onClick="exclude(<?= $curso['id'] ?>);" type="button" class="btn btn-danger" title="Desativar"><span class="glyphicon glyphicon-remove"></span></button>
-                                    <?php else: ?>
+										<?php if ($this->session->nivel == 1) :?>
+											<button type="button" class="btn btn-warning" title="Editar" data-toggle="modal" data-target="#exampleModal" data-whateverid="<?= $curso['id'] ?>" data-whateversigla="<?= $curso['sigla'] ?>" data-whatevernome="<?= $curso['nome'] ?>" data-whateversemestres="<?= $curso['qtdSemestres'] ?>" data-whatevergrau="<?= $curso['grau'] ?>" data-whateverperiodo="<?= $curso['idPeriodo'] ?>"><span class="glyphicon glyphicon-pencil"></span></button>
+											<button onClick="exclude(<?= $curso['id'] ?>);" type="button" class="btn btn-danger" title="Desativar"><span class="glyphicon glyphicon-remove"></span></button>
+										 <?php endif; ?>
+										 <?php if ($this->session->nivel == 2) :?>
+											<button type="button" class="btn btn-primary" title="Editar" data-toggle="modal" data-target="#exampleModal" data-whateverid="<?= $curso['id'] ?>" data-whateversigla="<?= $curso['sigla'] ?>" data-whatevernome="<?= $curso['nome'] ?>" data-whateversemestres="<?= $curso['qtdSemestres'] ?>" data-whatevergrau="<?= $curso['grau'] ?>" data-whateverperiodo="<?= $curso['idPeriodo'] ?>"><span class="glyphicon glyphicon-eye-open"></span></button>
+										 <?php endif; ?>
+								  <?php else: ?>
                                         <button onClick="able(<?= $curso['id'] ?>)" type="button" class="btn btn-success delete" title="Ativar"><span class="glyphicon glyphicon-ok"></span></button>
                                     <?php endif; ?>
                                 </td>
@@ -283,6 +290,118 @@
 					<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
 				</div>
 
+				<?= form_close() ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Aqui é o Modal2 de alteração dos cursos-->
+<div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content col-md-12">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="exampleModalLabel">Cursos</h4>
+            </div>
+            <div class="modal-body">
+				
+				<?= form_open('Curso/atualizar') ?>
+				
+				<div class="form-group">
+					<input type="hidden" name="cursoId" value="" id="recipient-id">
+				</div>
+				
+				<div class="row">
+					<div class="form-group col-md-12">
+						<?= form_label('Nome:', 'recipient-nome', array('class' => 'control-label')) ?>
+						<?= form_input('nomeCurso', set_value('nomeCurso'), array('class' => 'form-control', 'id' => 'recipient-nome', 'required' => 'required' , 'readonly' => 'readonly')) ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-6 margin-top-error">
+						<?= form_error('nomeCurso') ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="form-group col-md-3">
+						<?= form_label('Sigla:', 'recipient-sigla', array('class' => 'control-label')) ?>
+						<?= form_input('cursoSigla', set_value('cursoSigla'), array('class' => 'form-control', 'id' => 'recipient-sigla', 'maxlength' => '5', 'required' => 'required' , 'readonly' => 'readonly')) ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-6 margin-top-error">
+						<?= form_error('cursoSigla') ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-5">
+						<?= form_label('Quantidade de Semestres:', 'recipient-semestres', array('class' => 'control-label')) ?>
+					</div>
+				</div> 
+				
+				<div class="row">
+					<div class="form-group col-md-2">
+						<?= form_input('cursoQtdSemestres', set_value('cursoQtdSemestres'), array('pattern' => '[0-9]+$', 'maxlength' => '2', 'id' => 'recipient-semestres', 'class' => 'form-control', 'required' => 'required' , 'readonly' => 'readonly')) ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-11 margin-top-error">
+						<?= form_error('cursoQtdSemestres') ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<!-- DropListPeriodo (Droplist) -->
+					<div class="form-group col-md-9">
+						<label>Período:</label>
+						<?= form_dropdown('cursoPeriodos[]', $periodo, null, array('id' => 'cursoPeriodos', 'multiple' => 'multiple')) ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-10 margin-top-error">
+						<?= form_error('cursoPeriodos[]') ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<!-- DropListGrau (Droplist) -->
+					<div class="form-group col-md-7">
+						<label for="curso-name" class="control-label">Grau:</label>
+						<?= form_dropdown('cursoGrau', $graus, null, array('class' => 'form-control' , 'disabled' => 'disabled')) ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-10 margin-top-error">
+						<?= form_error('cursoGrau') ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="form-group col-md-9">
+						<label>Disciplinas:</label>
+						<?= form_dropdown('cursoDisciplinas[]', $disciplinas, null, array('id' => 'cursoDisciplinas', 'multiple' => 'multiple')) ?>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-10 margin-top-error">
+						<?= form_error('cursoDisciplinas[]') ?>
+					</div>
+				</div>
+
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+				</div>
+			
 				<?= form_close() ?>
             </div>
         </div>
