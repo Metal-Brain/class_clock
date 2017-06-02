c29c2e7725093fe742d0a5119f7ca1aedfec8dce
felipertec <felipe.rtec@gmail.com>
2017-06-02 08:10:16 -0300

    Agora pode selecionar quantos horários quiser!
    Realizado a alteração se der errado voltar a branch

--- application/views/disponibilidade/js_disponibilidades.php
+++ application/views/disponibilidade/js_disponibilidades.php
@@ -48,31 +48,6 @@
 			}
 		});
 
-		$('select[name=dia]').change(function () {
-			var day = $(this).val();
-			var url = '<?= base_url('index.php/Professor/verificaDisponibilidade/') ?>'+day;
-
-			$.getJSON(url,function (data) {
-				if (data.result == false) {
-					var msg = '<div id="disponibilidade-msg" class="text-center alert alert-danger">'+
-					'<p><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Não há disponibilidade para o dia selecionado.</p>'
-					'</div>';
-					$('#content').prepend(msg);
-
-					setTimeout(function () {
-						$('#disponibilidade-msg').hide('slow');
-					},2.5 * 1000);
-
-					$('select[name=dia]').val(0);
-				}
-			}).fail(function () {
-				var msg = '<div class="text-center alert alert-danger">'+
-				'<p><span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span> Não foi possível verificar a disponibilidade do dia selecionado. Atualize a página e tente novamente.</p>'+
-				'</div>';
-				$('#content').prepend(msg);
-			});
-		});
-
 	});
 </script>
 
