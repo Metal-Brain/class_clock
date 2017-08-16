dbec39a8365c854750fbfa84c64021d3ebddab09
felipertec <felipe.rtec@gmail.com>
2017-08-16 16:32:28 -0300

    Adicionado o formulario de Turnos

---
+++ application/views/turnos/turnos.php
@@ -0,0 +1,36 @@
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12" style="background: green; height: 60px;"></div>
+			</div>
+		</div>
+
+		<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-12">
+						<form>
+								<div class="col-xs-12 col-sm-12 col-md-12 form-group">
+										<div class="col-xs-12 col-sm-12 col-md-12" style="height: 40px;"></div>
+										<label>Nome:</label>
+										<input class="form-control" placeholder="Nome">
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12 form-group">
+										<label>Quantidade de aulas:</label>
+										<input class="form-control"  placeholder="Informe a quantidade">
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12 form-group">
+									<label >Horario de entrada da aula:</label>
+									<select class="form-control">
+										<option>Selecionar</option>
+										<option>07:00 às 12:00</option>
+										<option>13:00 às 18:00</option>
+										<option>19:00 às 23:00</option>
+									</select>
+								</div>
+								<div class="col-xs-12 col-sm-12 col-md-12 form-group">
+									<button type="button" class="btn btn-primary btn-lg active" >Cadastrar</button>
+								</div>
+								
+						</form>
+					</div>
+				</div>
+		</div>
