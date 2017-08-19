d969928820cd2449ca43e23da0284097904207b9
Felipe R <felipe.rtec@gmail.com>
2017-08-18 22:22:07 -0300

    template configurado

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,36 +1,19 @@
-<div class="row">
-	<div id="footer" class="col">
-		<p>Desenvolvido por Metal Code</p>
-	</div>
-</div>
+<footer class="text-center">
+			 <div class="container">
+					 <p>
 
-</div><!--Fecha container-fluid-->
+							 IFSP
+					 </p>
+					 <p>
+							 123 - CEP: 15161-216 - Telefone: +55 (16) 5122-0626
+					 </p>
+					 <p class="text-center">
+							 Desenvolvimento ACME & Brains Working
+					 </p>
+			 </div>
+	 </footer>
+	 <script src="<?= base_url('assets/js/jquery-3.1.1.min.js')?>"></script>
+	 <script src="<?= base_url('assets/js/bootstrap.min.js')?>"></script>
+</body>
 
-<script type="text/javascript" src="<?= base_url('assets/js/jquery-3.1.1.min.js') ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/jquery.validate.min.js') ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/js/bootbox.min.js') ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/DataTables/datatables.min.js') ?>"></script>
-<script type="text/javascript" src="<?= base_url('assets/JqueryMask/jquery.mask.min.js') ?>"></script>
-<!-- multi-select -->
-<script type="text/javascript" src="<?= base_url('assets/multi-select/js/jquery.multi-select.js') ?>"></script>
-<script type="text/javascript">
-	// override jquery validate plugin defaults
-	$.validator.setDefaults({
-		highlight: function(element) {
-			$(element).closest('.form-group').addClass('has-error');
-		},
-		unhighlight: function(element) {
-			$(element).closest('.form-group').removeClass('has-error');
-		},
-		errorElement: 'span',
-		errorClass: 'help-block',
-		errorPlacement: function(error, element) {
-			if(element.parent('.input-group').length) {
-				error.insertAfter(element.parent());
-			} else {
-				error.insertAfter(element);
-			}
-		}
-	});
-</script>
+</html>
