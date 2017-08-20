21ea70bf9a929ebfad9c78a45f704ad489194763
LucasLeonel21 <leonel.lucas@hotmail.com>
2017-08-20 13:54:42 -0300

    Adicionado Modelo Model por conta do eloquent.

---
+++ application/models/Model.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+    use Illuminate\Database\Eloquent\Model as Eloquent;
+
+    class Model extends Eloquent {
+        public $timestamps = false;
+    }
+
+?>
