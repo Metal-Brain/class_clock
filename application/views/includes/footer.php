30869285935920e9458334dc951fed1a6c2874ce
felipertec <felipe.rtec@gmail.com>
2017-08-20 18:50:35 -0300

    Add caminho do script js

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -14,6 +14,7 @@
 	 </footer>
 	 <script src="<?= base_url('assets/js/jquery-3.1.1.min.js')?>"></script>
 	 <script src="<?= base_url('assets/js/bootstrap.min.js')?>"></script>
+	 <script src="<?= base_url('assets/js/script.js')?>"></script>
 </body>
 
 </html>
