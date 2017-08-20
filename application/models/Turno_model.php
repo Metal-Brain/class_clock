d7814355025e398f9e3402d412c2fae8da05f7dd
Caio de Freitas <caiof.adriano@hotmail.com>
2017-08-20 10:40:19 -0300

    Realizada pequena modificação no modelo turno

--- application/models/Turno_model.php
+++ application/models/Turno_model.php
@@ -1,11 +1,12 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-    /** 
+    use Illuminate\Database\Eloquent\Model;
+
+    /**
      * Esta classe é um modelo do banco de dados que representa os turnos que a instituição possui
      * @author Lucas Leonel
      * @since 2017/08/19
     */
-
     class Turno_model extends Model{
 
         protected $table = 'turno';
