a35e3b1813b3a88ece55e35f1ca774fbb53f6e9e
jeanfbd <jeanfbd@hotmail.com>
2017-08-20 13:12:53 -0300

    Criado branch de Modalidade

---
+++ application/controllers/Grau.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *  Essa classe é responsavel por todas regras de negócio sobre Grau.
+ * @author Jean Brock
+ * @since 2017/08/20
+ */
+class Grau extends CI_Controller {
+  function index () {
+  }
+  function cadastrar() {
+    // Criando regra de validação do formulário
+    $this->form_validation->set_rules('nome_grau','nome',array('required','max_length[20]','trim','strtolower'));
+    // Setando os delimitadores da mensagem de erro.
+    $this->form_validation->set_error_delimiters('<span class="text-danger">','</span>');
+    if ( $this->form_validation->run()) {
+      $grau = array(
+        'nome_grau'      => $this->input->post('nome_grau')
+      );
+    } else {
+      $this->load->view('grau/grau');
+    }
+  }
+  function editar(){
+
+  }
+}
+?>
