cb31727d86b1091ed08de92c3715ec986086f085
Caio de Freitas <caiof.adriano@hotmail.com>
2017-08-21 11:59:15 -0300

    modificações em turno

--- application/controllers/Turno.php
+++ application/controllers/Turno.php
@@ -7,28 +7,47 @@ class Turno extends CI_Controller {
 
   function index () {
 
+    $turnos = Turno_model::all();
+
+    $this->load->template('turnos/turnos',compact('turnos'));
   }
 
   function cadastrar () {
 
     $this->form_validation->set_rules('nome_turno','nome',array('required','max_length[25]','trim','strtolower'));
-    $this->form_validation->set_rules('inicio','horário de entrada',array('required'));
     $this->form_validation->set_error_delimiters('<span class="text-danger">','</span>');
 
     if ($this->form_validation->run()) {
       $this->salvar();
     } else {
-      $this->load->template('turnos/turnos');
+      $turnos = Turno_model::all();
+      $this->load->template('turnos/turnos',compact('turnos'));
     }
 
   }
 
-  function salvar () {
+  private function salvar () {
     // TODO: recuperar os dados do formulário e fazer a persistencia dos dados.
+
+    $turno['nome_turno']  = $this->input->post('nome_turno');
+    $turno['horario']     = $this->input->post('horario');
+
   }
 
   function editar () {
+    $this->load->template('turnos/turnosEditar');
+  }
 
+  private function atualizar () {
+
+    $turno['nome']    = $this->input->post('nome_turno');
+    $turno['horario'] = $this->input->post('horario');
+
+    // TODO: Fazer a atualização dos dados no banco
+  }
+
+  function deletar ($id) {
+    // TODO: alterar o status do turno no banco de dados
   }
 }
 
