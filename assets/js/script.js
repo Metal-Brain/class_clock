bfbd0e411d37d9044591903cf005e74fc7f7c52f
FELIPE <FELIPE>
2017-08-21 22:17:10 -0300

    Atualizado o formulario de turnos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+	var num = 2;
 	$('#btnAdd').click(function(){
-		var a = '<div> <label>horario de entrada:</label> <input name="horario[]" class="form-control" type="time"></div>';
-		var b = '<div> <label>horario de saida:</label> <input name="horario[]" class="form-control" type="time"></div>';
-		$('#copi').append(a);
-		$('#copi2').append(b);
+		var a = '<div class="row aux"><div class="col-xs-12 col-sm-12 col-md-1"><label style="padding: 8px 0 0 0;">Aula '+ num +'</label></div><div class="col-xs-12 col-sm-12 col-md-2 form-group"><input name="horario[]" class="form-control hora" type="text" placeholder="Início" minlength="5" maxlength="5"></div><div class="col-xs-12 col-sm-12 col-md-2 form-group"><input name="horario[]" class="form-control hora" type="text" placeholder="Fim" minlength="5" maxlength="5"></div></div>';
+		$('.aux').last().after(a);
+		num++;
+		$('.hora').mask('00:00');
 	});
 
-
 });
+
+$(document).ready(function(){
+	$('.hora').mask('00:00');
+});
+
