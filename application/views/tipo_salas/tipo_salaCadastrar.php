9e2900903d383d4d580305a4e7eecb2b975373dc
Albino Freitas <albino_freitas@hotmail.com>
2017-08-30 13:01:46 -0300

    alteração link de envio do formulário

--- application/views/tipo_salas/tipo_salaCadastrar.php
+++ application/views/tipo_salas/tipo_salaCadastrar.php
@@ -1,6 +1,6 @@
 <div class="container col-xs-12 col-sm-12 col-md-10 col-lg-10" style="padding-top: 5px">
 	
-	<form id="formTipo_Sala" action="<?= site_url('tipo_sala/cadastrar')?>" method="post">
+	<form id="formTipo_Sala" action="<?= site_url('tipo_sala/salvar')?>" method="post">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-5 form-group">
 				<label>Nome:</label>
