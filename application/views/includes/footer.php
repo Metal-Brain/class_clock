9aea796c7eeaf17750add65b1bd878807a22d036
urielcaire <uriel.caire@gmail.com>
2017-09-05 15:46:04 -0300

    add active p/ menu

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -6,7 +6,7 @@
 			<p class="text-center">Desenvolvimento Metal Brain</p>
 		</footer>
 	</div>
-	
+
 </div>
 <script src="<?= base_url('assets/js/jquery-3.1.1.min.js')?>"></script>
 <script src="<?= base_url('assets/js/bootstrap.min.js')?>"></script>
@@ -14,3 +14,4 @@
 <script src="<?= base_url('assets/js/jquery.validate.min.js')?>"></script>
 <script type="text/javascript" src="<?= base_url('assets/js/jquery.validate.traducao.js')?>" charset="utf-8"></script>
 <script src="<?= base_url('assets/DataTables/datatables.js')?>"></script>
+<script src="<?= base_url('assets/js/main.js')?>"></script>
