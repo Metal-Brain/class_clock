3505c9a75db18c6a72d544ccfefb15cada9aa753
Albino Freitas <albino_freitas@hotmail.com>
2017-09-05 16:36:21 -0300

    Refatoração do controller adicionando comentários ao código e algumas correções

--- application/controllers/Tipo_sala.php
+++ application/controllers/Tipo_sala.php
@@ -1,53 +1,72 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- *
+ * @author AlbinoFreitas
  */
 class Tipo_sala extends CI_Controller {
 
+  /*
+  * Retorna uma lista com todos os tipo salas ativos
+  */
   function index () {
-      $tipo_salas = TipoSala_model::all();
-      $this->load->template('tipo_salas/tipo_sala', compact('tipo_salas'), 'tipo_salas/js_tipo_sala');
+    $tipo_salas = TipoSala_model::all();
+    $this->load->template('tipo_salas/tipo_sala', compact('tipo_salas'), 'tipo_salas/js_tipo_sala');
   }
 
+  /*
+  * Redireciona para o formulário de cadastro
+  */
   function cadastrar () {
     $this->load->template('tipo_salas/tipo_salaCadastrar');
   }
 
-  function salvar () {
+  /*
+  * Persiste os dados do formulário
+  */
+  function salvar(){
+    // Criando regra de validação do formulário
+    $this->form_validation->set_rules('nome_tipo_sala','nome',array('required','max_length[30]','trim', 'is_unique[tipo_sala.nome_tipo_sala]'));
+    $this->form_validation->set_rules('descricao_tipo_sala','descrição',array('required','max_length[254]','trim'));
+    // Setando os delimitadores da mensagem de erro.
+    $this->form_validation->set_error_delimiters('<span class="text-danger">','</span>');
 
-    $this->validar();
-
-    if ( $this->form_validation->run() ) {
-  
-      $dados = array(
+    if( $this->form_validation->run() ){
+      try{
+        $dados = [
           'nome_tipo_sala'      => $this->input->post('nome_tipo_sala'),
           'descricao_tipo_sala' => $this->input->post('descricao_tipo_sala')
-      );
+        ];
 
-      TipoSala_model::firstOrCreate($dados);
-    
+        TipoSala_model::firstOrCreate($dados);
+      }catch(Exception $e){
+        $this->session->set_flashdata('danger','Problemas ao cadastrar o tipo de sala, tente novamente!');
+      }
     }else{
       $this->session->set_flashdata('danger','Problemas ao cadastrar o tipo de sala, tente novamente!');
     }
 
     redirect('Tipo_sala');
-
   }
 
+  /*
+  * Redireciona para o formulário de edição
+  */
   function editar($id){
     $tipo_salas = TipoSala_model::findOrFail($id);
-
     $this->load->template('tipo_salas/tipo_salaEditar', compact('tipo_salas', 'id'), 'tipo_salas/js_tipo_sala');
   }
 
+  /*
+  * Atualiza os dados no banco
+  */
   public function atualizar($id){
+    // Criando regra de validação do formulário
     $this->form_validation->set_rules('nome_tipo_sala','nome',array('required','max_length[30]','trim'));
     $this->form_validation->set_rules('descricao_tipo_sala','descrição',array('required','max_length[254]','trim'));
     // Setando os delimitadores da mensagem de erro.
     $this->form_validation->set_error_delimiters('<span class="text-danger">','</span>');
 
-    if ( $this->form_validation->run() ) {
+    if( $this->form_validation->run() ){
       try{
           $dados = array(
             'nome_tipo_sala'      => $this->input->post('nome_tipo_sala'),
@@ -66,18 +85,12 @@ class Tipo_sala extends CI_Controller {
     redirect('Tipo_sala');
   }
 
+  /*
+  * Deleta (desativa) o tipo sala passado
+  */
   public function deletar($id){
     TipoSala_model::findOrFail($id)->delete();
-
     redirect('Tipo_sala');
-  }
-
-  private function validar(){
-    // Criando regra de validação do formulário
-    $this->form_validation->set_rules('nome_tipo_sala','nome',array('required','max_length[30]','trim', 'is_unique[tipo_sala.nome_tipo_sala]'));
-    $this->form_validation->set_rules('descricao_tipo_sala','descrição',array('required','max_length[254]','trim'));
-    // Setando os delimitadores da mensagem de erro.
-    $this->form_validation->set_error_delimiters('<span class="text-danger">','</span>');
   }
 
 }
