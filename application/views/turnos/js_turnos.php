5c4f0b906452a49a98eb275fb4770cc8022fd4a1
urielcaire <uriel.caire@gmail.com>
2017-09-05 15:27:25 -0300

    removendo funcoes fora de uso do js

--- application/views/turnos/js_turnos.php
+++ application/views/turnos/js_turnos.php
@@ -78,43 +78,10 @@
 			});
 
 		});
-	</script>
-
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('#TurnoTable').DataTable();
-	} );
-	var horarioIndex = 2;
-	var aula = 2;
-
-	//função para adicionar horário
-	$('#btnAdd').click(function(){
-		var content = '<div class="row aux"><div class="col-xs-12 col-sm-12 col-md-1"><label style="padding: 8px 0 0 0;">Aula '+ aula +'</label></div><div class="col-xs-12 col-sm-12 col-md-2 form-group"><input name="horario['+ horarioIndex +']" class="form-control hora" type="text" placeholder="Início" minlength="5" maxlength="5" required></div>';
-		horarioIndex++
-		content += '<div class="col-xs-12 col-sm-12 col-md-2 form-group"><input name="horario['+ horarioIndex +']" class="form-control hora inicio" type="text" placeholder="Fim" minlength="5" maxlength="5" required></div></div>';
-		$('.aux').last().after(content);
-		aula++;
-		horarioIndex++;
-		mask();
-	});
-
-
-	//função para remover o horário
-	$('#btnRemove').click(function(){
-			if(aula > 2){
-			$('.aux:last').remove();
-			aula--;
+		function mask() {
+			$('.hora').mask('00:00');
 		}
-	});
-
-	$('.salvar').click(function(){
-		$('.aux').val();
-	});
-
-	function mask() {
-		$('.hora').mask('00:00');
-	}
-	mask();
+		mask();
 </script>
 <script type="text/javascript">
 	$.validator.addClassRules({
