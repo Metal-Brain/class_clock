692b4d253b540646d2faa1481b1de040ea41b583
Caio de Freitas <caiof.adriano@hotmail.com>
2017-09-08 17:54:55 -0300

    Realizada correções em turnos

--- application/views/turnos/turnosCadastrar.php
+++ application/views/turnos/turnosCadastrar.php
@@ -8,24 +8,14 @@
 				<?= form_error('nome_turno') ?>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" id="turnoCadastrar">
 			<div class="col-xs-12 col-sm-12 col-md-12 form-group">
 				<button id="btnAdd" type="button" class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> Adicionar Aula</button>
 				<button id="btnRemove" type="button" class="btn btn-danger add-field"><span class="glyphicon glyphicon-minus"></span> Remover Aula</button>
 			</div>
 		</div>
-		<div class="row aux" id="linha">
-			<div class="col-xs-12 col-sm-12 col-md-1">
-				<label style="padding: 8px 0 0 0;">Aula 1</label>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-2 form-group">
-				<input id="inicio" name="horario[0]" class="form-control hora" type="text" placeholder="Início">
-				<?= form_error('horario[]') ?>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-2 form-group">
-				<input id="fim" name="horario[1]" class="form-control hora" type="text" placeholder="Fim">
-				<?= form_error('horario[]') ?>
-			</div>
+		<div id="horarios">
+
 		</div>
 		<div class="row">
 			<div class="col-md-12 form-group">
