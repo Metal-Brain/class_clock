747a9633eeb3e9f51e5199b4061d114d4ad7316a
Pliavi <pliavi.rpgm@gmail.com>
2017-09-11 14:54:00 -0300

    Utilização dos métodos facilitadores no controller PEssoa (TESTAR)

--- application/controllers/Pessoa.php
+++ application/controllers/Pessoa.php
@@ -1,6 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pessoa extends MY_Controller {
+    private $id_docente = 1;
 
     function index() {
         $pessoas = Pessoa_model::all();
@@ -8,26 +9,125 @@ class Pessoa extends MY_Controller {
     }
 
     function cadastrar() {
-        $this->load->template('pessoas/cadastrar';
+        $tipos = Tipo_model::all();
+        $this->load->template('pessoas/cadastrar', compact('tipos'));
     }
 
     function salvar() {
-        $pessoa = new Pessoa_model;
-        $pessoa->save();
+        DB::transaction(function() {
+            $has_docente = in_array($this->id_docente, $this->request('tipos')); // Se o docente estiver na lista de tipos
+
+            // testando!
+            $this->set_validations([
+                ['nome', 'nome', 'required|min_length[5]'],
+                ['prontuario', 'prontuário', 'required|is_unique[pessoa.prontuario]|exact_length[7]'],
+                ['senha', 'senha', 'required|min_length[6]'],
+                ['data_nascimento', 'data de nascimento', 'required|valid_date'],
+                ['email', 'email', 'required|valid_email'],
+            ]);
+
+            if($has_docente) {
+                $this->set_validation('data_ingresso_campus', 'data de ingresso no câmpus',
+                    'required|valid_date');
+                $this->set_validation('data_ingresso_ifsp', 'data de ingresso no IFSP',
+                    'required|valid_date');
+                $this->set_validation('area', 'área',
+                    'required');
+                $this->set_validation('regime_contrato', 'regime de contrato',
+                    'required|in_list[20, 40]');
+            }
+
+            $this->run_validation();
+
+            $pessoa = Pessoa_model::create([
+                'nome' => $this->request('nome'),
+                'prontuario' => $this->request('prontuario'),
+                'senha' => password_hash($this->request('senha'), PASSWORD_BCRYPT),
+                'data_nascimento' => $this->request('data_nascimento'),
+                'email' => $this->request('email'),
+            ]);
+
+            if($has_docente) {
+                $docente = Docente_model::create([
+                    'data_ingresso_campus' => $this->request('data_ingresso_campus'),
+                    'data_ingresso_ifsp' => $this->request('data_ingresso_ifsp'),
+                    'area' => $this->request('area'),
+                    'regime_contrato' => $this->request('regime_contrato'),
+                ]);
+            }
+
+            // Monta as relações de tipo
+            $pessoa->tipos()->sync($this->request('tipos'));
+
+            $this->session->set_flashdata('success', 'Pessoa cadastrada com sucesso');
+        });
     }
 
     function editar($id) {
         $pessoa = Pessoa_model::findOrFail($id);
-        $this->load->template('pessoas/editar', compact('pessoa'));
+        $tipos = Tipo_model::all();
+        $this->load->template('pessoas/editar', compact('pessoa', 'tipos'));
     }
 
     function atualizar($id) {
-        $pessoa = Pessoa_model::findOrFail($id);
-        $pessoa->save();
+        DB::transaction(function() use ($id) {
+            $pessoa = Pessoa_model::findOrFail($id);
+            $has_docente = in_array($this->id_docente, $this->request('tipos')); // Se o docente estiver na lista de tipos
+
+            $this->set_validation('nome', 'nome',
+                'required|min_length[5]');
+            $this->set_validation('prontuario', 'prontuário',
+                'required|exact_length[7]');
+            $this->set_validation('senha', 'senha',
+                'required|min_length[6]');
+            $this->set_validation('data_nascimento', 'data de nascimento',
+                'required|valid_date');
+            $this->set_validation('email', 'email',
+                'required|valid_email');
+
+            if($has_docente) {
+                $this->set_validation('data_ingresso_campus', 'data de ingresso no câmpus',
+                    'required|valid_date');
+                $this->set_validation('data_ingresso_ifsp', 'data de ingresso no IFSP',
+                    'required|valid_date');
+                $this->set_validation('area', 'área',
+                    'required');
+                $this->set_validation('regime_contrato', 'regime de contrato',
+                    'required|in_list[20, 40]');
+            }
+
+            $this->run_validation();
+
+            $pessoa->update([
+                'nome' => $this->request('nome'),
+                'prontuario' => $this->request('prontuario'),
+                'senha' => $this->request('senha'),
+                'data_nascimento' => $this->request('data_nascimento'),
+                'email' => $this->request('email'),
+            ]);
+
+            if($has_docente) {
+                $docente = $pessoa->docente;
+                $docente->update([
+                    'data_ingresso_campus' => $this->request('data_ingresso_campus'),
+                    'data_ingresso_ifsp' => $this->request('data_ingresso_ifsp'),
+                    'area' => $this->request('area'),
+                    'regime_contrato' => $this->request('regime_contrato'),
+                ]);
+            } else if(!is_null($pessoa->docente)){
+                $pessoa->docente->delete();
+            }
+
+            // Realinha os tipos
+            $pessoa->tipos()->sync($this->request('tipos'));
+
+            $this->session->set_flashdata('success', 'Pessoa atualizada com sucesso');
+        });
     }
 
     function deletar($id) {
         Pessoa_model::findOrFail($id)->delete();
+        $this->session->set_flashdata('success', 'Pessoa deletada com sucesso');
     }
 
 }
