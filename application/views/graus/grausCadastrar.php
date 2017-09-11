d3b6d8d10d0cc659dfa9690683eb7e82fabcaa15
KarolJosef <karoljozef123@hotmail.com>
2017-09-10 23:50:40 -0300

    Corrigido problemas relacionados ao CRUD da tabela de testes

--- application/views/graus/grausCadastrar.php
+++ application/views/graus/grausCadastrar.php
@@ -19,7 +19,7 @@
 
             <div class="form-group">
               <label>Codigo:</label>
-              <input type="number"
+              <input type="text"
                 class="form-control"
                 id="codigo"
                 name="codigo"
@@ -30,7 +30,7 @@
 
 			<div class="row">
 			<div class="col-md-12 form-group">
-				<a class="btn btn-danger active" href="<?= base_url('index.php/Grau')?>" style="float: right;"><span class="glyphicon glyphicon-remove"></span> Cancelar</a>
+				<a class="btn btn-danger active" href="<?= base_url('index.php/Grau')?>" style="float: right;"><span class="glyphicon glyphicon-remove"></span> Voltar</a>
 				<button type="submit" class="btn btn-success active salvar" style="float: right; margin-right: 10px;"><span class="glyphicon glyphicon-floppy-disk"></span> Salvar</button>
 			</div>
 		</div>
