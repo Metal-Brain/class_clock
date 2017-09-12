c87af302df46d230665aee25a549810bc800dee6
AlbinoFreitas <albino_freitas@hotmail.com>
2017-09-11 21:01:52 -0300

    Adição dos modelos de Pessoa, Area, Docente e Tipo.

---
+++ application/models/Pessoa_Model.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Pessoa_model extends Model{
+
+        protected $table = 'pessoa';
+        protected $guarded = [];
+
+        public docente(){
+            return $this->hasOne(Docente_model::class);
+        }
+
+    }
+
+?>
