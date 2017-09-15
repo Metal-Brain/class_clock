145dffeeb12223f70a9630d47d82ce3f2d1ec251
pliavi <pliavi.rpgm@gmail.com>
2017-09-15 15:57:24 -0300

    Tipo não extende model pois não está usando softdeletes

--- application/models/Tipo_model.php
+++ application/models/Tipo_model.php
@@ -1,6 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
-class Tipo_model extends Model{
+class Tipo_model extends Eloquent {
     protected $table = 'tipo_pessoa';
     protected $fillable = ['nome'];
+    public $timestamps = false;
 }
