1785a19352d8e0319f32c61ec6c120de07517fd4
pliavi <pliavi.rpgm@gmail.com>
2017-09-15 14:59:53 -0300

    Correções - corrigindo id do docente - adicionando withTrashed - adicionado comentários - corrigida as validações

--- application/controllers/Pessoa.php
+++ application/controllers/Pessoa.php
@@ -1,18 +1,30 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pessoa extends MY_Controller {
-    private $id_docente = 1;
+    private $id_docente = 4;
 
+    /**
+    * Página inicial de pessoa
+    * @author Vitor "Pliavi"
+    */
     function index() {
-        $pessoas = Pessoa_model::all();
+        $pessoas = Pessoa_model::withTrashed()->get();
         $this->load->template('pessoas/pessoas', compact('pessoas'));
     }
 
+    /**
+    * Formulário de cadastro
+    * @author Vitor "Pliavi"
+    */
     function cadastrar() {
-        $tipos = Tipo_model::all();
+        $tipos = Tipo_model::withTrashed()->get();
         $this->load->template('pessoas/cadastrar', compact('tipos'));
     }
 
+    /**
+    * Salva uma pessoa no banco
+    * @author Vitor "Pliavi"
+    */
     function salvar() {
         DB::transaction(function() {
             // Se o docente estiver na lista de tipos
@@ -20,16 +32,17 @@ class Pessoa extends MY_Controller {
 
             $this->set_validations([
                 ['nome', 'nome', 'required|min_length[5]'],
-                ['prontuario', 'prontuário', 'required|is_unique[pessoa.prontuario]|exact_length[7]'],
+                ['prontuario', 'prontuário', 'required|is_unique[pessoa.prontuario]|exact_length[6]'],
                 ['senha', 'senha', 'required|min_length[6]'],
-                ['data_nascimento', 'data de nascimento', 'required|valid_date'],
                 ['email', 'email', 'required|valid_email'],
+                ['tipos[]', 'tipos', 'required']
             ]);
 
             if($has_docente) {
                 $this->set_validations([
-                    ['data_ingresso_campus', 'data de ingresso no câmpus', 'required|valid_date'],
-                    ['data_ingresso_ifsp', 'data de ingresso no IFSP', 'required|valid_date'],
+                    ['nascimento', 'data de nascimento', 'required|valid_date'],
+                    ['ingresso_campus', 'data de ingresso no câmpus', 'required|valid_date'],
+                    ['ingresso_ifsp', 'data de ingresso no IFSP', 'required|valid_date'],
                     ['area', 'área', 'required'],
                     ['regime_contrato', 'regime de contrato', 'required|in_list[20, 40]'],
                 ]);
@@ -49,31 +62,42 @@ class Pessoa extends MY_Controller {
         });
     }
 
+    /**
+    * Formulário de edição
+    * @author Vitor "Pliavi"
+    * @param $id ID da pessoa a ser desativada
+    */
     function editar($id) {
         $pessoa = Pessoa_model::findOrFail($id);
         $tipos = Tipo_model::all();
         $this->load->template('pessoas/editar', compact('pessoa', 'tipos'));
     }
 
+    /**
+    * Atualiza os dados da pessoa
+    * @author Vitor "Pliavi"
+    * @param $id ID da pessoa a ser atualizada
+    */
     function atualizar($id) {
         DB::transaction(function() use ($id) {
             $pessoa = Pessoa_model::findOrFail($id);
             $has_docente = in_array($this->id_docente, $this->request('tipos')); // Se o docente estiver na lista de tipos
 
             $this->set_validations([
-                'nome', 'nome', 'required|min_length[5]',
-                'prontuario', 'prontuário', 'required|exact_length[7]',
-                'senha', 'senha', 'required|min_length[6]',
-                'data_nascimento', 'data de nascimento', 'required|valid_date',
-                'email', 'email', 'required|valid_email',
+                ['nome', 'nome', 'required|min_length[5]'],
+                ['prontuario', 'prontuário', 'required|exact_length[7]'],
+                ['senha', 'senha', 'required|min_length[6]'],
+                ['email', 'email', 'required|valid_email'],
+                ['tipos[]', 'tipos', 'required']
             ]);
 
             if($has_docente) {
                 $this->set_validations([
-                    'data_ingresso_campus', 'data de ingresso no câmpus', 'required|valid_date',
-                    'data_ingresso_ifsp', 'data de ingresso no IFSP', 'required|valid_date',
-                    'area', 'área', 'required',
-                    'regime_contrato', 'regime de contrato', 'required|in_list[20, 40]',
+                    ['nascimento', 'data de nascimento', 'required|valid_date'],
+                    ['ingresso_campus', 'data de ingresso no câmpus', 'required|valid_date'],
+                    ['ingresso_ifsp', 'data de ingresso no IFSP', 'required|valid_date'],
+                    ['area', 'área', 'required'],
+                    ['regime_contrato', 'regime de contrato', 'required|in_list[20, 40]'],
                 ]);
             }
 
@@ -94,6 +118,11 @@ class Pessoa extends MY_Controller {
         });
     }
 
+    /**
+    * Desativa a pessoa
+    * @author Vitor "Pliavi"
+    * @param $id ID da pessoa a ser desativada
+    */
     function deletar($id) {
         Pessoa_model::findOrFail($id)->delete();
         $this->session->set_flashdata('success', 'Pessoa deletada com sucesso');
