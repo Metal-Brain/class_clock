363a3201c425d10dcd06a6436168660da59ab441
KAROL <KAROL>
2017-09-15 22:31:59 -0300

    Corrigido quase todos os erros so resta o ultimo da tabela

--- application/controllers/Grau.php
+++ application/controllers/Grau.php
@@ -16,7 +16,7 @@ class Grau extends CI_Controller {
         $this->load->template('graus/graus', compact('graus'),'graus/js_graus');
     }
 
-    /**     
+    /**
     * Formulário de cadastro
     * @author Jean Brock | Vitor Silvério | Thalita Barbosa
     */
@@ -43,22 +43,24 @@ class Grau extends CI_Controller {
                                                  'integer',
                                                  'greater_than[0]',
                                                  'max_length[5]',
-                                                 'is_unique[grau.codigo]')
+                                                 'is_unique[grau.codigo]'
+                                                 )
                                          );
         $this->form_validation->set_error_delimiters('<span class="text-danger">','</span>');
 
         if($this->form_validation->run()){
             try {
-                                                       
+
                 $grau = new Grau_model();
                 <?=htmlspecialchars($grau->nome_grau)?> = $this->input->post('nome_grau');
                 $grau->codigo = $this->input->post('codigo');
-                $grau->save();     
-                                
+                $grau->save();
+
                 $this->session->set_flashdata('success','Grau cadastrado com sucesso');
-        }
-             catch (Exception $ignored){}
                 redirect('Grau');
+
+            } catch (Exception $ignored){}
+            redirect('Grau/cadastrar');
         }
 
         $this->session->set_flashdata('danger','Problemas ao cadastrar o grau, tente novamente!');
@@ -89,7 +91,7 @@ class Grau extends CI_Controller {
 												'alpha_numeric_spaces')
                                          );
         $this->form_validation->set_rules('codigo',
-                                          'codigo', 
+                                          'codigo',
                                           array('required',
                                                 'integer',
                                                 'greater_than[0]',
@@ -132,7 +134,7 @@ class Grau extends CI_Controller {
 
         redirect('Grau');
     }
-    
+
     /**
     * Ativa o Grau
     * @author Thalita Barbosa
--- application/views/graus/graus.php
+++ application/views/graus/graus.php
@@ -1,19 +1,19 @@
-<div class="container col-md-10 col-lg-10">
+<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
   <!-- Alertas de sucesso / erro -->
-  <div class="row">
-    <div class="col-lg-offset-3 col-lg-6">
+  <div class="row" style="margin-top: 5px;">
+    <div class="col-md-12">
       <?php if ($this->session->flashdata('success')) : ?>
         <div class="alert alert-success">
-          <p class="text-center"><?= $this->session->flashdata('success') ?></p>
+          <p><span class="glyphicon glyphicon-ok-sign"></span> <?= $this->session->flashdata('success') ?></p>
         </div>
       <?php elseif ($this->session->flashdata('danger')) : ?>
         <div class="alert alert-danger">
-          <p class="text-center"><?= $this->session->flashdata('danger') ?></p>
+          <p><span class="glyphicon glyphicon-remove-sign"></span> <?= $this->session->flashdata('danger') ?></p>
         </div>
       <?php endif; ?>
     </div>
   </div>
-
+  
   <!-- Início do conteúdo da view-->
  <div class="top-bar" style="padding: 0 0 15px 0">
     <div class="row">
@@ -47,11 +47,13 @@
         <td class="text-center"><?= ( empty($grau->deletado_em) ) ? 'Ativado' : 'Desativado'?></td>
           <td class="text-center">
 
-            <a class="btn btn-warning glyphicon glyphicon-pencil" title="Editar" href="<?= site_url('grau/editar/'.$grau->id)?>"></a>
+            
             
             <?php if ( empty($grau->deletado_em) ) : ?> 
-              <button class="btn btn-danger" type="button" id="btn-delete" onclick="confirmDelete(<?= $grau->id ?>,'Deseja desativar a modalidade?','deletar')"> <i class="glyphicon glyphicon-remove"></i></button>
+              <a class="btn btn-warning glyphicon glyphicon-pencil" title="Editar" href="<?= site_url('grau/editar/'.$grau->id)?>"></a>
+              <button class="btn btn-danger" type="button" id="btn-delete" onclick="confirmDelete(<?= $grau->id ?>,'Deseja desativar a modalidade?','deletar')" > <i class="glyphicon glyphicon-remove"></i></button>
             <?php else : ?> 
+            <a class="btn btn-warning glyphicon glyphicon-pencil disabled" title="Editar" href="<?= site_url('grau/editar/'.$grau->id)?>"></a>
             <button class="btn btn-success" type="button" id="btn-delete" onclick="confirmDelete(<?= $grau->id ?>,'Deseja ativar a modalidade?','ativar')"> <i class="glyphicon glyphicon-check"></i></button>
             <?php endif; ?>
         </td>
