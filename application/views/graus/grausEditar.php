5c0338e07a7948ebbe1b609ea03ebfb767f259cb
KarolJosef <karoljozef123@hotmail.com>
2017-09-18 21:30:40 -0300

    Corrigido os erros mencionados na tabelas de testes]

--- application/views/graus/grausEditar.php
+++ application/views/graus/grausEditar.php
@@ -23,7 +23,7 @@
                 class="form-control"
                 id="codigo"
                 name="codigo"
-                value="<?= $grau->codigo?>"
+                value=" <?=htmlspecialchars($grau->nome_grau)?> "
                 />
             </div>
 
