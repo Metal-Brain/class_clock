638010a907aae3e3a428b9eecebd82587e84950a
YASMIN <YASMIN>
2017-09-18 21:37:46 -0300

    Atualização do pessoa

--- application/views/pessoas/editar.php
+++ application/views/pessoas/editar.php
@@ -14,7 +14,7 @@
 
 		<div class="form-group width-180">
 			<label>Senha:</label>
-			<input id="senha" name="senha" class="form-control" type="text" minlength="6">
+			<input id="senha" name="senha" class="form-control" type="password" minlength="6">
 			<?= form_error('senha') ?>
 		</div>
 
