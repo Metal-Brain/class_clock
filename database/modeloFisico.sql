12b72b3949f0ef585879f612084bc14d83148372
nikolasagl <nikolasagl@hotmail.com>
2017-09-20 17:35:49 -0300

    Banco atualizado

--- database/modeloFisico.sql
+++ database/modeloFisico.sql
@@ -22,30 +22,20 @@ CREATE SCHEMA IF NOT EXISTS `horario` DEFAULT CHARACTER SET latin1 ;
 USE `horario` ;
 
 -- -----------------------------------------------------
--- Table `horario`.`grau`
+-- Table `horario`.`modalidade`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `horario`.`grau` ;
+DROP TABLE IF EXISTS `horario`.`modalidade` ;
 
-CREATE TABLE IF NOT EXISTS `horario`.`grau` (
+CREATE TABLE IF NOT EXISTS `horario`.`modalidade` (
   `id` TINYINT(4) NOT NULL AUTO_INCREMENT,
-  `nome_grau` VARCHAR(50) NOT NULL,
-  `codigo` INT NOT NULL,
-  `deletado_em` TIMESTAMP NULL,
+  `nome_modalidade` VARCHAR(50) NOT NULL,
+  `codigo` INT(11) NOT NULL,
+  `deletado_em` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `codigo_UNIQUE` (`codigo` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
--- -----------------------------------------------------
--- Table `horario`.`tipo_sala`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `horario`.`tipo_sala` (
-  `id` TINYINT NOT NULL AUTO_INCREMENT,
-  `nome_tipo_sala` VARCHAR(30) NOT NULL,
-  `descricao_tipo_sala` VARCHAR(254) NOT NULL,
-  `deletado_em` TIMESTAMP NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `horario`.`curso`
@@ -54,18 +44,18 @@ DROP TABLE IF EXISTS `horario`.`curso` ;
 
 CREATE TABLE IF NOT EXISTS `horario`.`curso` (
   `id` SMALLINT(6) NOT NULL AUTO_INCREMENT,
-  `grau_id` TINYINT NOT NULL,
+  `modalidade_id` TINYINT(4) NOT NULL,
   `codigo_curso` CHAR(5) NOT NULL,
   `nome_curso` VARCHAR(75) NOT NULL,
   `sigla_curso` CHAR(3) NOT NULL,
   `qtd_semestre` TINYINT(2) NOT NULL,
   `fechamento` CHAR(1) NOT NULL,
-  `deletado_em` TIMESTAMP NULL,
+  `deletado_em` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_curso_grau1_idx` (`grau_id` ASC),
-  CONSTRAINT `fk_curso_grau1`
-    FOREIGN KEY (`grau_id`)
-    REFERENCES `horario`.`grau` (`id`)
+  INDEX `fk_curso_modalidade1_idx` (`modalidade_id` ASC),
+  CONSTRAINT `fk_curso_modalidade1`
+    FOREIGN KEY (`modalidade_id`)
+    REFERENCES `horario`.`modalidade` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -95,12 +85,13 @@ DROP TABLE IF EXISTS `horario`.`disciplina` ;
 CREATE TABLE IF NOT EXISTS `horario`.`disciplina` (
   `id` SMALLINT(6) NOT NULL AUTO_INCREMENT,
   `curso_id` SMALLINT(6) NOT NULL,
-  `tipo_sala_id` TINYINT NOT NULL,
+  `tipo_sala_id` TINYINT(4) NOT NULL,
   `nome_disciplina` VARCHAR(50) NOT NULL,
   `sigla_disciplina` CHAR(5) NOT NULL,
+	`modulo` TINYINT(2) NOT NULL,
   `qtd_professor` TINYINT(1) NOT NULL,
   `qtd_aulas` TINYINT(2) NOT NULL,
-  `deletado_em` TIMESTAMP NULL,
+  `deletado_em` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_disciplina_curso1_idx` (`curso_id` ASC),
   INDEX `fk_disciplina_tipo_sala` (`tipo_sala_id` ASC),
@@ -124,11 +115,12 @@ DROP TABLE IF EXISTS `horario`.`turno` ;
 CREATE TABLE IF NOT EXISTS `horario`.`turno` (
   `id` TINYINT(4) NOT NULL AUTO_INCREMENT,
   `nome_turno` VARCHAR(25) NOT NULL,
-  `deletado_em` TIMESTAMP NULL,
+  `deletado_em` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `nome_turno_UNIQUE` (`nome_turno` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+
 
 -- -----------------------------------------------------
 -- Table `horario`.`horario`
@@ -140,7 +132,7 @@ CREATE TABLE IF NOT EXISTS `horario`.`horario` (
   `turno_id` TINYINT(4) NOT NULL,
   `inicio` TIME NOT NULL,
   `fim` TIME NOT NULL,
-  `deletado_em` TIMESTAMP NULL,
+  `deletado_em` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_horario_turno_idx` (`turno_id` ASC),
   CONSTRAINT `fk_horario_turno`
@@ -151,14 +143,168 @@ CREATE TABLE IF NOT EXISTS `horario`.`horario` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
-INSERT INTO grau(nome_grau, codigo) VALUES("Tecnologia", "00001");
-INSERT INTO grau(nome_grau, codigo) VALUES("Bacharel", "00021");
-INSERT INTO grau(nome_grau, codigo) VALUES("Pos Graduação", "00231");
-INSERT INTO grau(nome_grau, codigo) VALUES("Mestrado", "001321");
-INSERT INTO grau(nome_grau, codigo) VALUES("Doutorado", "44121");
- 
-INSERT INTO curso(grau_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento) VALUES(1,"123", "Análise e Desenvolvimento de Sistemas", "ADS", 6, "S");
-INSERT INTO curso(grau_id, codigo_curso,nome_curso, sigla_curso, qtd_semestre, fechamento) VALUES(2,"123",  "Processos Gerenciais", "PRG", 8, "S");
-INSERT INTO curso(grau_id,codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento) VALUES(3,"123",  "Fisica", "FIS", 4, "S");
-INSERT INTO curso(grau_id, codigo_curso,nome_curso, sigla_curso, qtd_semestre, fechamento) VALUES(4,"123",  "Computação Avançada", "CPA", 3, "B");
-INSERT INTO curso(grau_id,codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento) VALUES(5,"123",  "Cura do Cancer", "CDC", 8, 'S');
+
+-- -----------------------------------------------------
+-- Table `horario`.`tipo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `horario`.`tipo` ;
+
+CREATE TABLE IF NOT EXISTS `horario`.`tipo` (
+  `id` TINYINT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(25) NOT NULL,
+  `deletado_em` TIMESTAMP NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `horario`.`area`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `horario`.`area` ;
+
+CREATE TABLE IF NOT EXISTS `horario`.`area` (
+  `id` SMALLINT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `horario`.`pessoa`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `horario`.`pessoa` ;
+
+CREATE TABLE IF NOT EXISTS `horario`.`pessoa` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(150) NOT NULL,
+  `prontuario` CHAR(6) NOT NULL,
+  `senha` CHAR(64) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `deletado_em` TIMESTAMP NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `horario`.`docente`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `horario`.`docente` ;
+
+CREATE TABLE IF NOT EXISTS `horario`.`docente` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `pessoa_id` INT NOT NULL,
+  `area_id` SMALLINT NOT NULL,
+  `nascimento` DATE NOT NULL,
+  `ingresso_campus` DATE NOT NULL,
+  `ingresso_ifsp` DATE NOT NULL,
+  `regime` CHAR(1) NOT NULL,
+  `deletado_em` TIMESTAMP NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_docente_area1_idx` (`area_id` ASC),
+  INDEX `fk_docente_pessoa1_idx` (`pessoa_id` ASC),
+  CONSTRAINT `fk_docente_area1`
+    FOREIGN KEY (`area_id`)
+    REFERENCES `horario`.`area` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_docente_pessoa1`
+    FOREIGN KEY (`pessoa_id`)
+    REFERENCES `horario`.`pessoa` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `horario`.`tipo_pessoa`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `horario`.`tipo_pessoa` ;
+
+CREATE TABLE IF NOT EXISTS `horario`.`tipo_pessoa` (
+  `tipo_id` TINYINT NOT NULL,
+  `pessoa_id` INT NOT NULL,
+  PRIMARY KEY (`tipo_id`, `pessoa_id`),
+  INDEX `fk_tipo_has_pessoa_pessoa1_idx` (`pessoa_id` ASC),
+  INDEX `fk_tipo_has_pessoa_tipo1_idx` (`tipo_id` ASC),
+  CONSTRAINT `fk_tipo_has_pessoa_tipo1`
+    FOREIGN KEY (`tipo_id`)
+    REFERENCES `horario`.`tipo` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tipo_has_pessoa_pessoa1`
+    FOREIGN KEY (`pessoa_id`)
+    REFERENCES `horario`.`pessoa` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Inserção de dados
+-- -----------------------------------------------------
+
+INSERT INTO tipo(nome) VALUES ('Administrador'), ('CRA'), ('DAE'), ('Docente');
+
+INSERT INTO tipo_sala(nome_tipo_sala, descricao_tipo_sala) VALUES("Teorica", "Mesas e Cadeiras");
+INSERT INTO tipo_sala(nome_tipo_sala, descricao_tipo_sala) VALUES("Pratica", "Computadores");
+INSERT INTO tipo_sala(nome_tipo_sala, descricao_tipo_sala) VALUES("Hibrida", "Mesas, cadeiras e computadores");
+INSERT INTO tipo_sala(nome_tipo_sala, descricao_tipo_sala) VALUES("Auditorio", "Apresentacoes");
+INSERT INTO tipo_sala(nome_tipo_sala, descricao_tipo_sala) VALUES("VideoConferencia", "Videoconferencias ao vivo");
+
+INSERT INTO modalidade(nome_modalidade, codigo) VALUES("Tecnologia", "00001");
+INSERT INTO modalidade(nome_modalidade, codigo) VALUES("Bacharel", "00021");
+INSERT INTO modalidade(nome_modalidade, codigo) VALUES("Pos Graduação", "00231");
+INSERT INTO modalidade(nome_modalidade, codigo) VALUES("Mestrado", "001321");
+INSERT INTO modalidade(nome_modalidade, codigo) VALUES("Doutorado", "44121");
+
+<<<<<<< HEAD
+INSERT INTO curso(grau_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(1, 111, "Análise e Desenvolvimento de Sistemas", "ADS", 6, "B");
+INSERT INTO curso(grau_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(2, 222, "Processos Gerenciais", "PRG", 8, "S");
+INSERT INTO curso(grau_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(3, 333, "Fisica", "FIS", 4, "S");
+INSERT INTO curso(grau_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(4, 444, "Computação Avançada", "CPA", 3, "B");
+INSERT INTO curso(grau_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+=======
+INSERT INTO curso(modalidade_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(1, 111, "Análise e Desenvolvimento de Sistemas", "ADS", 6, "B");
+INSERT INTO curso(modalidade_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(2, 222, "Processos Gerenciais", "PRG", 8, "S");
+INSERT INTO curso(modalidade_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(3, 333, "Fisica", "FIS", 4, "S");
+INSERT INTO curso(modalidade_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+    VALUES(4, 444, "Computação Avançada", "CPA", 3, "B");
+INSERT INTO curso(modalidade_id, codigo_curso, nome_curso, sigla_curso, qtd_semestre, fechamento)
+>>>>>>> 39f03e8b87e418b71d66c2cab4fc72bf37748ca0
+    VALUES(5, 544, "Cura do Cancer", "CDC", 8, "B");
+
+INSERT INTO disciplina(curso_id, tipo_sala_id, nome_disciplina, sigla_disciplina, qtd_professor, qtd_aulas)
+    VALUES(1, 5, "Análise de Sistemas", "ADS", 2, 4);
+INSERT INTO disciplina(curso_id, tipo_sala_id, nome_disciplina, sigla_disciplina, qtd_professor, qtd_aulas)
+    VALUES(2, 4, "Matemática", "MAT", 1, 6);
+INSERT INTO disciplina(curso_id, tipo_sala_id, nome_disciplina, sigla_disciplina, qtd_professor, qtd_aulas)
+    VALUES(3, 3, "Fisica Avancada", "FSA", 1, 8);
+INSERT INTO disciplina(curso_id, tipo_sala_id, nome_disciplina, sigla_disciplina, qtd_professor, qtd_aulas)
+    VALUES(4, 2, "Hardware", "HDW", 2, 2);
+INSERT INTO disciplina(curso_id, tipo_sala_id, nome_disciplina, sigla_disciplina, qtd_professor, qtd_aulas)
+    VALUES(5, 1, "Medicina", "MED", 2, 2);
+
+INSERT INTO turno(nome_turno) VALUES("Matutino");
+INSERT INTO turno(nome_turno) VALUES("Vespertino");
+INSERT INTO turno(nome_turno) VALUES("Noturno");
+INSERT INTO turno(nome_turno) VALUES("Integral");
+INSERT INTO turno(nome_turno) VALUES("Diário");
+
+INSERT INTO horario(turno_id, inicio, fim) VALUES(1, '9:10:00', '10:00');
+INSERT INTO horario(turno_id, inicio, fim) VALUES(2, '13:10:00', '14:00');
+INSERT INTO horario(turno_id, inicio, fim) VALUES(3, '20:10:00', '21:00');
+INSERT INTO horario(turno_id, inicio, fim) VALUES(4, '9:10:00', '10:00');
+INSERT INTO horario(turno_id, inicio, fim) VALUES(5, '10:10:00', '20:00');
+
+INSERT INTO area(id, nome) VALUES(1, "FIXME: Precisa colocar área no controller Pessoa!");
