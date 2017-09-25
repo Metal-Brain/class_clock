29219971211c13274ea1b77f28102f1e097e2390
Felipe <felipe.rtec@gmail.com>
2017-09-25 20:46:43 -0300

    Primeira parte
    começo da branch

---
+++ application/views/fpas/fpas.php
@@ -0,0 +1,9 @@
+<div class="col-md-10 col-lg-10">
+    <div style="padding: 0 0 15px 0">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="page-header">FPA</h2>
+            </div>
+        </div>
+    </div>
+</div>
