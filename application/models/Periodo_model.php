2ea1cbad1a9e5ece608d09142020e4f20691d780
Denny Azevedo <denny_af@yahoo.com.br>
2017-09-26 00:38:30 -0300

    Criando o Controller
    Criação do controller e ajustes do model

--- application/models/Periodo_model.php
+++ application/models/Periodo_model.php
@@ -1,19 +1,25 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-  
-  class Curso_model extends Model{
+/**
+ * periodos que a instituição possui
+ * @author Denny Azevedo (moodificado)
+ * @since 2017/10/25
+ */
+  class Periodo_model extends Model
+  {
     
-    protected $table = 'Periodo';
+    protected $table = 'periodo';
     protected $fillable = ['nome'];
 
     //Função responsável por retornas as disciplinas oferecidas do semestre.
-    public function disciplinaOferecida(){
-      return $this->hasMany(DisciplinaOferecida_model:class, 'periodo_id');
+    public function disciplinaOferecida()
+    {
+      return $this->hasMany(DisciplinaOferecida_model::class, 'periodo_id');
     }
 
     //Função responsável por retornar todos as FPAS referente ao período.
-    public function fpa(){
-      return $this->hasMany(Fpa_model:class, 'periodo_id');
+    public function fpa()
+    {
+      return $this->hasMany(Fpa_model::class, 'periodo_id');
     }
 
   }
-}
