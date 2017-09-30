e1805526f26b47109ff609d6f995eea2bdc2d961
pliavi <pliavi.rpgm@gmail.com>
2017-09-30 20:26:11 -0300

    trocado disciplinaoferecida por turma no modelo

--- application/models/Turma_model.php
+++ application/models/Turma_model.php
@@ -1,14 +1,25 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-/**
- * Esta classe é um modelo do banco de dados que representa as disciplinas que a instituição possui
- * @author Cleyton de Castro
- * @since 2017/08/26
-*/
 
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Turma_model extends Eloquent {
     protected $table = 'disciplina_oferecida';
-    protected $fillable = ['id', 'disciplina_id', 'periodo_id', 'turno_id', 'qtd_alunos', 'dp'];
+    protected $fillable = ['disciplina_id', 'periodo_id', 'turno_id', 'qtd_alunos', 'dp'];
     public $timestamps = false;
+
+    public function disciplina(){
+        return $this->belongsTo(Disciplina_model::class, 'disciplina_id');
+    }
+
+    public function periodo(){
+        return $this->belongsTo(Periodo_model::class, 'periodo_id');
+    }
+
+    public function turno(){
+        return $this->belongsTo(Turno_model::class, 'turno_id');
+    }
+
+    public function preferencias(){
+        return $this->belongsToMany(Fpa_model::class, 'preferencia', 'fpa_id', 'disciplinas_oferecidas_id')->withPivot('ordem');
+    }
 }
