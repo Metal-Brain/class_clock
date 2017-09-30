439e78e35eb8e28ed06a030d4d5e887151b09b52
LUCAS <LUCAS>
2017-09-29 21:31:40 -0300

    Correção da Identação do Modelo de Área.

--- application/models/Area_model.php
+++ application/models/Area_model.php
@@ -1,11 +1,11 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class Area_model extends model{
-        
-        protected $table = 'area';
-        protected $fillable = ['nome'];
+class Area_model extends model{
+    
+    protected $table = 'area';
+    protected $fillable = ['nome'];
 
-        public function docente(){
-            return $this->hasMany(Docente_model::class, 'area_id');
-        }
+    public function docente(){
+        return $this->hasMany(Docente_model::class, 'area_id');
     }
+}
