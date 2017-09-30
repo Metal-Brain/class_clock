68ee82442f82c8878bffed4256e578a9e9be366f
LUCAS <LUCAS>
2017-09-29 21:49:44 -0300

    Atualização do modelo de curso contendo a função de docente.

--- application/models/Curso_model.php
+++ application/models/Curso_model.php
@@ -1,35 +1,38 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+* Modelo responsável pelo curso
+* @author Lucas Leonel
+* @since 2017/08/26
+*/
+
+class Curso_model extends Model {
+
+  public $timestamps = false;
+  protected $table = 'curso';
+  protected $fillable = array('codigo', 'modalidade_id', 'codigo_curso', 'nome_curso', 'sigla_curso', 'qtd_semestre', 'fechamento');
+
+
   /**
-  * Modelo responsável pelo curso
+  * Função responsável para retornar o grau do curso
   * @author Lucas Leonel
   * @since 2017/08/26
   */
 
-  class Curso_model extends Model {
-
-    public $timestamps = false;
-    protected $table = 'curso';
-    protected $fillable = array('codigo', 'modalidade_id', 'codigo_curso', 'nome_curso', 'sigla_curso', 'qtd_semestre', 'fechamento');
-
-
-    /**
-    * Função responsável para retornar o grau do curso
-    * @author Lucas Leonel
-    * @since 2017/08/26
-    */
-
-    public function modalidade(){
-      return $this->belongsTo(Modalidade_Model::class, 'curso_id');
-    }
-
-    /**
-    * Função responsável para retornar as disciplinas do curso
-    * @author Lucas Leonel
-    * @since 2017/08/26
-    */
-
-    public function disciplinas(){
-      return $this->hasMany(Disciplinas_Model::class, 'curso_id');
-    }
+  public function modalidade(){
+    return $this->belongsTo(Modalidade_Model::class, 'curso_id');
   }
-?>
+
+  /**
+  * Função responsável para retornar as disciplinas do curso
+  * @author Lucas Leonel
+  * @since 2017/08/26
+  */
+
+  public function disciplinas(){
+    return $this->hasMany(Disciplinas_Model::class, 'curso_id');
+  }
+
+  public function docente(){
+    return $this->belongsTo(Docente_model::class, 'docente_id');
+  }
+}
