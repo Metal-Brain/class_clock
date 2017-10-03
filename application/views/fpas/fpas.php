2d1e25ebde97186c36354f2ca0ad2dfc855d57b0
KAROL <KAROL>
2017-10-02 22:36:48 -0300

    Incluido dataTable

--- application/views/fpas/fpas.php
+++ application/views/fpas/fpas.php
@@ -13,51 +13,29 @@
                 <h4><strong>Selecione o método desejado:</strong></h4>
             </div>
 
-              <div class="dropdown">
-                <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" style="width:171px; height:40px;">Selecionar
-                <span class="caret"></span></button>
-                <ul class="dropdown-menu">
-                    <li><a href="#">Disponibilidade</a></li>
-                    <li><a href="#">Indisponibilidade</a></li>
-                </ul>
-              </div>
+             
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-offset-1 col-md-10">
             <div class="table-responsive">
-              @loop -> turno
-              <table class="table">
+             
+              <table id="FpaTable" class="table table-striped">
                 <thead>
                   <tr>
-                    <!-- Abaixo ainda será decidido o que vai vir. add noturno como teste -->
-                    <th colspan="7" class="text-center">Noturno</th>
-                  </tr>
-                  <tr>
-                    <th>Horarios</th>
-                    <th>Segunda</th>
-                    <th>Terça</th>
-                    <th>Quarta</th>
-                    <th>Quinta</th>
-                    <th>Sexta</th>
-                    <th>Sabado</th>
+                    <th>Fpas</th>
                   </tr>
                 </thead>
                 <tbody>
-                  @loop -> horarios
                   <tr>
                     <td></td>
-                    <td><input type="checkbox" name="" value=""></td>
-                    <td><input type="checkbox" name="" value=""></td>
-                    <td><input type="checkbox" name="" value=""></td>
-                    <td><input type="checkbox" name="" value=""></td>
-                    <td><input type="checkbox" name="" value=""></td>
-                    <td><input type="checkbox" name="" value=""></td>
                   </tr>
                 </tbody>
+                    
+
               </table>
-              @endloop
+            
             </div>
           </div>
 
--- application/views/fpas/js_fpas.php
+++ application/views/fpas/js_fpas.php
@@ -3,4 +3,12 @@
 
     });
 </script>
+
+<script type="text/javascript">
+  $('#FpaTable').dataTable( {
+    "language": {
+      "url": "<?= base_url('assets/DataTables/translatePortuguese.js');?>"
+    }
+  } );
+</script>
 <!-- TODO Fazer as modificações>
