4dfc1ffda63431f8a61699818cb1de40010dbfed
Pliavi <pliavi.rpgm@gmail.com>
2017-10-08 20:14:43 -0300

    adicionado returno de turmas em turno

--- application/models/Turno_model.php
+++ application/models/Turno_model.php
@@ -7,6 +7,7 @@
     class Turno_model extends Model {
         protected $table = 'turno';
         protected $fillable = ['nome_turno'];
+
         /**
          * Retorna todos os horarios linkados com o turno ordenados pela hora de início
          * @author Lucas Leonel
@@ -15,6 +16,16 @@
         public function horarios() {
             return $this->hasMany(Horario_model::class, 'turno_id')->orderBy('horario.inicio', 'asc');
         }
+
+        /**
+         * Retorna todos as turmas linkadas com o turno ordenadas pelo turno;
+         * @author Lucas Leonel
+         * @since 2017/08/19
+        */
+        public function turmas() {
+            return $this->hasMany(Turma::class, 'turno_id')->orderBy('turno_id', 'asc');
+        }
+
         /**
          * Retorna a quantidade de horarios de um Turno
          * @author Uriel Cairê Balan Calvi
