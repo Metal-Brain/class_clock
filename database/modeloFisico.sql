65a69e4ac275492d2d316236ccba9e2be91167ba
cleytoncm <cleytoncm@gmail.com>
2017-10-24 21:53:21 -0200

    Correção INSERTpessoa/docente

--- database/modeloFisico.sql
+++ database/modeloFisico.sql
@@ -519,7 +519,7 @@ INSERT INTO pessoa(id, nome, prontuario, senha, email) VALUES(1, "Adminsitrador"
 INSERT INTO pessoa(id, nome, prontuario, senha, email) VALUES(2, "Docente 1", "151521", "$2y$10$2uOth8K7IW1YGSuCvAQDVe.6W53RSCOJWji9eCNTqN3DSfTHr.5oe
 ", "joao@ifsp.edu");
 
-INSERT INTO docente(id, pessoa_id, area_id, nascimento, ingresso_campus, ingresso_ifsp, regime) VALUES(1, 1, 1, "1996/08/11", "2007/08/11", "2007/08/11", 1);
+INSERT INTO docente(id, pessoa_id, area_id, nascimento, ingresso_campus, ingresso_ifsp, regime) VALUES(1, 2, 1, "1996/08/11", "2007/08/11", "2007/08/11", 1);
 
 INSERT INTO tipo_pessoa(tipo_id, pessoa_id) VALUES(1, 1), (4, 2);
 
