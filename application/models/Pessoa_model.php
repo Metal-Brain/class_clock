75658f91c7111accf684d79f182e5cb6699b4700
Vitor "Pliavi" Silvério de Souza <Pliavi.rpgm@gmail.com>
2017-10-31 21:21:53 -0200

    recolocando o pessoa_model.php

---
+++ application/models/Pessoa_model.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Pessoa_model extends Model{
+
+        protected $table = 'pessoa';
+        protected $fillable = [ "nome", "prontuario", "senha", "email" ];
+
+        /**
+        * Pega a senha do usuário e passa pelo blowfish assim que seu valor é dado.
+        * @author Vitor "Pliavi"
+        * @param $senha senha a ser "hasheada"
+        */
+        public function setSenhaAttribute($senha) {
+            $this->attributes['senha'] = password_hash($senha, PASSWORD_BCRYPT);
+        }
+
+        public function setNomeAttribute($nome) {
+            $this->attributes['nome'] = ucwords(mb_strtolower($nome, "utf-8"));
+        }
+
+        public function docente() {
+            return $this->hasOne(Docente_model::class, 'pessoa_id');
+        }
+
+        public function tipos() {
+            return $this->belongsToMany(Tipo_model::class, 'tipo_pessoa', 'pessoa_id', 'tipo_id');
+        }
+
+    }
