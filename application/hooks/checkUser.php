f00bacfc88757f8e5c4cd29ca5cce8f203d4c1c5
jeanfbd <jeanfbd@hotmail.com>
2017-11-06 20:42:07 -0200

    Update checkUser.php
    Corrigido acesso de disciplinas para administrador.

--- application/hooks/checkUser.php
+++ application/hooks/checkUser.php
@@ -63,6 +63,7 @@ class checkUser {
             'curso'       => ['index', 'cadastrar', 'salvar', 'editar', 'atualizar', 'deletar', 'ativar'],
             'pessoa'      => ['index', 'cadastrar', 'salvar', 'editar', 'atualizar', 'deletar', 'ativar'],
             'area'        => ['index', 'cadastrar', 'salvar', 'editar', 'atualizar', 'deletar', 'ativar'],
+						'disciplina'  => ['index', 'cadastrar', 'salvar', 'editar', 'atualizar', 'deletar', 'ativar'],
             'turma'       => ['index', 'cadastrar', 'salvar', 'editar', 'atualizar', 'deletar', 'ativar']
         ];
 
