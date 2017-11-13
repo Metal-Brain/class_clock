cebc87a62c6df0c07798d4a2ee6755e373a287cb
BRUNO <BRUNO>
2017-11-13 20:41:16 -0200

    Criado a função confirmDelete

--- application/views/turmas/js_turmas.php
+++ application/views/turmas/js_turmas.php
@@ -1,5 +1,25 @@
 <script type="text/javascript">
   $('#TurmaTable').dataTable();
+
+  function confirmDelete(id, msg, funcao) {
+				bootbox.confirm({
+					message: msg,
+					buttons: {
+						confirm: {
+							label: 'Sim',
+							className: 'btn-success'
+						},
+						cancel: {
+							label: 'Não',
+							className: 'btn-danger'
+						}
+					},
+					callback: function (result) {
+					if (result == true)
+						window.location.href = '<?= site_url("turma/") ?>' + funcao + '/' + id;
+					}
+				});
+			}
 </script>
 
 </body>
