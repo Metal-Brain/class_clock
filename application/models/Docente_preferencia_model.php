07eaf04c614ac1c29edb6ce109d0127bbecd5025
GILBERTO <GILBERTO>
2017-11-21 20:28:47 -0200

    recolocado eloquente no model docente_preferencia

--- application/models/Docente_preferencia_model.php
+++ application/models/Docente_preferencia_model.php
@@ -1,15 +1,14 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * Esta classe é um modelo do banco de dados que representa os horários que o turno possui
-<<<<<<< HEAD
  * @author Vitor Silvério
  * @since 2017/11/13
 */
+use Illuminate\Database\Eloquent\Model as Eloquent;
 
-class Docente_preferencia_model extends Model{
+class Docente_preferencia_model extends Eloquent{
     protected $table = 'docente_preferencia';
     public $timestamps = false;
     protected $fillable = ['docente_id', 'nome', 'nome_disciplina', 'nome_curso', 'curso_id'];
 
 }
-
