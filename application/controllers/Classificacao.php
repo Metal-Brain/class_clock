710c4cc5b613ac7dbbcaf03ebb454ed259c301cc
felipemrk2e <felipemrk2e@gmail.com>
2017-11-21 22:06:26 -0200

    Alteração no controller

--- application/controllers/Classificacao.php
+++ application/controllers/Classificacao.php
@@ -3,23 +3,20 @@
 
 class Classificacao extends MY_Controller {
     
-    public function index(){
+    public function index2(){
         $curso = $this->request('curso');
         
         $classificacao = DB::select("call classificacao(?)", [$curso]);
     }
 
-    public function index2() {
+    public function index() {
         $curso = $this->request('curso');
 
-        $classificacao = Classificacao_model::where('curso_id', $curso)
+        $classificacoes = Classificacao_model::where('curso_id', $curso)
         //aqui tu pode colocar qualquer where, orderby ou a p#rra toda
         ->get();
-
-        // Permite a utilização da notação de objeto ou array ao invés de apenas array
-        foreach($classificacao as $index => $cla) {
-            echo "{$index} => {$cla->nome}, {$cla->nome_curso}, {$cla->nome_disciplina}<br>";
-        }
+		
+	  $this->load->template('classificacoes/classificacao', compact('classificacoes'), 'classificacoes/js_classificacao');
     }
 
 }
