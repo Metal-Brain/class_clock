691bccd0e58ed5e2d132b0216700c9e4959a6786
Pliavi <pliavi.rpgm@gmail.com>
2017-11-27 19:57:06 -0200

    Update Classificacao

--- application/controllers/Classificacao.php
+++ application/controllers/Classificacao.php
@@ -2,21 +2,39 @@
 
 
 class Classificacao extends MY_Controller {
-    
-    public function index2(){
-        $curso = $this->request('curso');
-        
-        $classificacao = DB::select("call classificacao(?)", [$curso]);
-    }
 
     public function index() {
+        $user  = $this->CI->session->userdata('usuario_logado');
         $curso = $this->request('curso');
 
-        $classificacoes = Classificacao_model::where('curso_id', $curso)
-        //aqui tu pode colocar qualquer where, orderby ou a p#rra toda
-        ->get();
+        if (!is_null($user)) {
+            
+            $user = Pessoa_model::find($user['id']);
+            
+            if(!is_null($user->docente)) {
+                $docente = $user->docente;
+                $cursos = $docente->cursos;
+                
+                if($cursos->isEmpty()){
+                    throw new Exception("Docente não é coordenador de nenhum curso");
+                }
+                
+            }
+            
+            # id 3 é o do DAE
+            if($user->tipo->id == 3) {
+                $cursos = Curso_model::all();
+            }
+        } else {
+            throw new Exception("Não foi possível recuperar nenhum usuário logado");
+        }
+
+        $classificacoes = [];
+        if(!is_null($curso)){
+            $classificacoes = Classificacao_model::where('curso_id', $curso)->get();
+        }
 		
-	  $this->load->template('classificacoes/classificacao', compact('classificacoes'), 'classificacoes/js_classificacao');
+	  $this->load->template('classificacoes/classificacao', compact('classificacoes', 'cursos'), 'classificacoes/js_classificacao');
     }
 
 }
