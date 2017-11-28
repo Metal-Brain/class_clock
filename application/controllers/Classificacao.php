54e79ac77887e15be22c3ab299aa367530a735b8
Pliavi <pliavi.rpgm@gmail.com>
2017-11-27 22:50:57 -0200

    colocando a classificação direta do coordenador

--- application/controllers/Classificacao.php
+++ application/controllers/Classificacao.php
@@ -5,6 +5,7 @@ class Classificacao extends MY_Controller {
 
     public function index() {
         $user  = $this->session->userdata('usuario_logado');
+        $tipo  = $user['tipo'];
         $curso = $this->request('curso');
 
         if (!is_null($user)) {
@@ -13,16 +14,17 @@ class Classificacao extends MY_Controller {
             
             if(!is_null($user->docente)) {
                 $docente = $user->docente;
-                $cursos = $docente->cursos;
+                $cursos  = $docente->cursos;
                 
                 if($cursos->isEmpty()){
                     throw new Exception("Docente não é coordenador de nenhum curso");
                 }
-                
+
+                $curso = $cursos[0]; // Pega o curso que o docente coordena
             }
             
             # id 3 é o do DAE
-            if($user->tipo == 3) {
+            if($tipo == 3) {
                 $cursos = Curso_model::all();
             }
         } else {
@@ -34,7 +36,7 @@ class Classificacao extends MY_Controller {
             $classificacoes = Classificacao_model::where('curso_id', $curso)->get();
         }
 		
-	  $this->load->template('classificacoes/classificacao', compact('classificacoes', 'cursos'), 'classificacoes/js_classificacao');
+	    $this->load->template('classificacoes/classificacao', compact('classificacoes', 'cursos', 'tipo', 'user'), 'classificacoes/js_classificacao');
     }
 
 }
