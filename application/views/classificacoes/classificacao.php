1a947d489beae7d7e7dcdf5ca7ad5a9f2450621a
Pliavi <pliavi.rpgm@gmail.com>
2017-12-05 22:08:42 -0200

    correção em classificação

--- application/views/classificacoes/classificacao.php
+++ application/views/classificacoes/classificacao.php
@@ -1,47 +1,34 @@
 <?php $tipoUsuario = $this->session->userdata('usuario_logado')['tipos']; ?>
 
-
+ 
 <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
-
+	
 
 	<!-- Início do conteúdo da view-->
 	<div class="top-bar" style="padding: 0 0 15px 0">
 		<div class="row">
 			<div class="col-md-12">
-			<h2 class="page-header">Classificação
-
-			</h2>
+			<h2 class="page-header">Classificação</h2>
 			</div>
 		</div>
-		<?php if (in_array(1, $tipoUsuario)) :?>
-			<div class="row">
-				<h4 class="page-header">Selecione o curso desejado.</h4>
-				<form action="" method="GET">
-					<select class="form-control" name="curso" onchange="this.form.submit()">
-            <option disabled selected>Selecione um curso...</option>
-						<?php
-
-						foreach($cursos as $curso){
-								if($classificacoes[0]->curso_id == $curso['id']){
-
-							echo '<option value="'. $curso->id .'" selected>'.$curso->nome_curso.'</option>';
-
-
-						}else{
-							echo '<option value="'. $curso->id .'">'.$curso->nome_curso.'</option>';
-
-						}
-															}
-
-
-						?>
-
-					 </select>
-
-				</form>
-			</div>
-		<?php endif; ?>
 	</div>
+
+	<?php if (!$cursos->isEmpty()) :?>
+		<form method="POST">
+			<select name="curso">
+				<?php foreach ($cursos as $curso): ?>
+					<?php if($curso == $classificacoes[0]->curso_id): ?>
+						<option value="<?= $curso['id'] ?>">
+							<?= htmlspecialchars(ucwords($curso['nome_curso'])); ?>
+						</option>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			</select>
+			<button class="">Selecionar Curso</button>
+		</form>
+	<?php endif; ?>
+	<br>
+
 	<table id="classificacaoTable" class="table table-striped">
 		<thead>
 			<tr>
@@ -52,30 +39,15 @@
 		</thead>
 
 		<tbody>
-		<?php if (in_array(1, $tipoUsuario)) :?>
 			<?php foreach ($classificacoes as $curso) { ?>
-
+				
 					<td class="text-center"><?= htmlspecialchars(ucwords($curso['nome'])); ?></td>
 					<td class="text-center"><?= htmlspecialchars(ucwords($curso['nome_curso'])); ?></td>
 					<td class="text-center"><?= htmlspecialchars(ucwords($curso['nome_disciplina'])); ?></td>
-
+					
 			    </tr>
-
+			
 			<?php } ?>
-		<?php endif; ?>
-		<?php if (in_array(4, $tipoUsuario)) :?>
-			<?php foreach ($cursos as $curso) { ?>
-
-					<td class="text-center"><?= htmlspecialchars(ucwords($curso['nome'])); ?></td>
-					<td class="text-center"><?= htmlspecialchars(ucwords($curso['nome_curso'])); ?></td>
-					<td class="text-center"><?= htmlspecialchars(ucwords($curso['nome_disciplina'])); ?></td>
-
-			    </tr>
-
-			<?php } ?>
-		<?php endif; ?>
-		</tbody>
-	</table>
-
-
+			</tbody>
+		</table>
 </div>
