9e441fbed9f2be91600f139db24847c02eef5f6d
VITOR <VITOR>
2017-12-08 21:28:39 -0200

    Corrigido controller Classificação: Não mexa mais nessa caralha!

--- application/controllers/Classificacao.php
+++ application/controllers/Classificacao.php
@@ -5,35 +5,35 @@ class Classificacao extends MY_Controller {
     public function index() {
         $user  = $this->session->userdata('usuario_logado');
         $tipos  = $user['tipos'];
-        $curso = $this->request('curso');
+        $curso = $cursoPesquisado = $this->request('curso');
 		$cursos = Curso_model::all();
-		$cursoPesquisado = $this->request('curso');
             
 		$user = Pessoa_model::find($user['id']);
 		
-		if(!is_null($user->docente)) {
-			$docente = $user->docente;
-			$cursos  = $docente->cursos;
-			
-			if($cursos->isEmpty()){
-				$this->session->set_flashdata("É necessário ser coordenador de algum curso para acessar a classificação");
-				redirect('/'); // Docente nao é coordenador
-			}
-
-			$curso = $cursos[0]; // Pega o curso que o docente coordena
-			$cursos = Classificacao_model::where('curso_id', $curso->id)->get();
-		}
 		
 		# id 3 é o do DAE
 		if(in_array(3, $tipos)) {
 			$cursos = Curso_model::all();
-		}        
+		} else {
+			if(!is_null($user->docente)) {
+				$docente = $user->docente;
+				$cursos  = $docente->cursos;
+				
+				if($cursos->isEmpty()){
+					$this->session->set_flashdata("danger", "É necessário ser coordenador de algum curso para acessar a classificação");
+					redirect('/'); // Docente nao é coordenador
+				}
+
+				$curso = $cursos[0]; // Pega o curso que o docente coordena
+				$cursos = Classificacao_model::where('curso_id', $curso->id)->get();
+			}
+		}
 
         $classificacoes = [];
         if(!is_null($curso)){
             $classificacoes = Classificacao_model::where('curso_id', $curso)->get();
         }
 		
-	    $this->load->template('classificacoes/classificacao', compact('classificacoes', 'cursos','cursoPesquisado', 'tipos', 'user'), 'classificacoes/js_classificacao');
+	    $this->load->template('classificacoes/classificacao', compact('classificacoes', 'cursos', 'cursoPesquisado', 'tipos', 'user'), 'classificacoes/js_classificacao');
     }
 }
